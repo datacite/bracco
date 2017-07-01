ae3003d4e288ec7c8256b33fd28e04eb5a0cad64
Martin Fenner <martin.fenner@datacite.org>
2017-07-01 17:09:45 +0200

    clean up package.json

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,20 +1,4 @@
 import Ember from 'ember';
-// import Cookie from 'ember-cli-js-cookie';
 
 export default Ember.Controller.extend({
-  // session: Ember.inject.service('session'),
-  //
-  // actions: {
-  //   authenticate() {
-  //     let jwt = Cookie.get('_datacite_jwt');
-  //     var a = this.get('session').authenticate('authenticator:cookie', jwt);
-  //     console.log(a)
-  //     return a
-  //   }
-  // }
-//   actions: {
-//     invalidateSession() {
-//       this.get('session').invalidate();
-//     }
-//   }
 });
