e8da8191a8f2c0fddd7e54aad9d884a86a63709f
Martin Fenner <mfenner@datacite.org>
2017-08-22 12:42:34 +0200

    removed obsolete initializer

--- app/initializers/inject-notifications.js
+++
@@ -1,8 +0,0 @@
-export function initialize(application) {
-  application.inject('controller', 'notifications', 'service:notification-messages');
-}
-
-export default {
-  name: 'inject-notifications',
-  initialize
-};
