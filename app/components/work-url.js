d4b64171660ca59522c2f14fc8c72d6769c65028
Martin Fenner <mfenner@datacite.org>
2017-08-22 22:52:09 +0200

    use profiles not api as member api

--- app/components/work-url.js
+++ app/components/work-url.js
@@ -19,20 +19,22 @@ export default Ember.Component.extend({
     checkLink() {
       let self = this;
       let url = this.get('model').get("identifier");
-      let result = fetch(url, {
-      	method: 'head',
+      fetch(url, {
+        method: 'head',
         timeout: 5000
       }).then(function(response) {
         if (response.ok) {
           self.set('status', 'ok');
           self.set('buttonType', 'success');
         } else {
-          console.log(response)
+          Ember.Logger.assert(false, response)
+
           self.set('status', 'failed');
           self.set('buttonType', 'warning');
         }
       }).catch(function(error) {
-        console.log(error)
+        Ember.Logger.assert(false, error)
+
         self.set('status', 'failed');
         self.set('buttonType', 'warning');
       });
