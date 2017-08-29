3ba5b2079e6d6ab94e252d4396cad9dfe73d210b
Martin Fenner <mfenner@datacite.org>
2017-08-29 16:14:12 +0200

    disable afterModel hooks for now

--- app/routes/data-centers/show.js
+++ app/routes/data-centers/show.js
@@ -3,10 +3,17 @@ import RouteMixin from 'ember-cli-pagination/remote/route-mixin';
 import { CanMixin } from 'ember-can';
 
 export default Ember.Route.extend(CanMixin, RouteMixin, {
-
   model(params) {
     return this.store.findRecord('data-center', params.data_center_id, { include: 'member' });
   },
+
+  // afterModel(model, transition) {
+  //   if (!this.can('read data-center', model)) {
+  //     let home = this.get('currentUser').get('home');
+  //     this.transitionTo(home);
+  //   }
+  // },
+
   actions: {
     queryParamsDidChange: function() {
       this.refresh();
--- app/routes/works/show.js
+++ app/routes/works/show.js
@@ -10,6 +10,13 @@ export default Ember.Route.extend(CanMixin, RouteMixin, {
     return this.store.findRecord('work', params.work_id, { include: 'member,data-center' });
   },
 
+  // afterModel(model, transition) {
+  //   if (!this.can('read work', model)) {
+  //     let home = this.get('currentUser').get('home');
+  //     this.transitionTo(home);
+  //   }
+  // },
+
   actions: {
     queryParamsDidChange: function() {
       this.refresh();
