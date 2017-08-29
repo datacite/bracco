a11638b4dfafa5ee81ef46f429f5c03762fb5374
Martin Fenner <mfenner@datacite.org>
2017-08-29 02:11:58 +0200

    refactor footer as component

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,22 +1,4 @@
 import Ember from 'ember';
-import ENV from 'bracco/config/environment';
-import { CanMixin } from 'ember-can';
 
-export default Ember.Route.extend(CanMixin, {
-  model() {
-    var url = ENV.CDN_URL + "/data/links.json"
-
-    return Ember.$.getJSON(url).then(function(data) {
-      if (ENV.environment === 'development') {
-        data.header_links = data.development_links;
-        return data;
-      } else if (ENV.environment === 'stage') {
-        data.header_links = data.stage_links;
-        return data;
-      } else {
-        data.header_links = data.production_links;
-        return data;
-      }
-    });
-  }
+export default Ember.Route.extend({
 });
