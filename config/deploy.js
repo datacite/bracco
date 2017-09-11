09fd1301f1b5e9bc84db9a3c335caa3ec1416492
Martin Fenner <mfenner@datacite.org>
2017-09-11 17:24:35 +0200

    deploy index.html to s3

--- config/deploy.js
+++ config/deploy.js
@@ -5,7 +5,8 @@ module.exports = function(deployTarget) {
     accessKeyId: process.env.AWS_ACCESS_KEY,
     secretAccessKey: process.env.AWS_SECRET_KEY,
     bucket: process.env.AWS_BUCKET,
-    region: process.env.AWS_REGION
+    region: process.env.AWS_REGION,
+    filePattern: '**/*.{js,css,png,gif,ico,jpg,map,xml,txt,svg,swf,eot,ttf,woff,woff2,html}'
   };
 
   return ENV;
