95f4f6b923fc5498e12641da6c476671c9c9d315
Martin Fenner <mfenner@datacite.org>
2017-09-14 01:41:24 +0200

    fix permissions for editing users

--- app/abilities/user.js
+++ app/abilities/user.js
@@ -5,6 +5,8 @@ export default Ability.extend({
   canCreate: Ember.computed('currentUser.role_id', 'currentUser.id', 'model.id', function() {
     switch(this.get('currentUser.role_id')) {
       case 'staff_admin':
+      case 'provider_admin':
+      case 'client_admin':
         return this.get('currentUser.uid') !== this.get('model.id');
       default:
         return false;
@@ -14,6 +16,10 @@ export default Ability.extend({
     switch(this.get('currentUser.role_id')) {
       case 'staff_admin':
         return true;
+        case 'provider_admin':
+          return this.get('currentUser.provider_id') === this.get('model.provider.id');
+        case 'client_admin':
+          return this.get('currentUser.client_id') === this.get('model.client.id');
       default:
         return false;
     }
