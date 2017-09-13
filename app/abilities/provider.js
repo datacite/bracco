d8993e518c3807627ffd21e708f228813c012842
Martin Fenner <mfenner@datacite.org>
2017-09-14 01:52:40 +0200

    fixed permissions to edit provider

--- app/abilities/provider.js
+++ app/abilities/provider.js
@@ -10,10 +10,12 @@ export default Ability.extend({
         return false;
     }
   }),
-  canUpdate: Ember.computed('currentUser.role_id', function() {
+  canUpdate: Ember.computed('currentUser.role_id', 'currentUser.provider_id', 'model.id', function() {
     switch(this.get('currentUser.role_id')) {
       case 'staff_admin':
         return true;
+      case 'provider_admin':
+        return this.get('currentUser.provider_id') === this.get('model.id');
       default:
         return false;
     }
