7b887b4c390e4696b9b01cd58dc81e3f0eaaf4ee
Martin Fenner <mfenner@datacite.org>
2017-09-26 18:16:06 +0200

    change sort labels

--- app/components/model-search.js
+++ app/components/model-search.js
@@ -59,10 +59,13 @@ export default Ember.Component.extend(RouteMixin, {
                          'user': 'User' }
     this.set('modelName', placeholders[this.get('model').get("modelName")]);
     if (this.get('model').get("modelName") === "doi") {
-      this.set('formats', { '-created': 'Sort by Registration Date', 'score': 'Sort by Relevance' });
+      this.set('formats', { '-created': 'Sort by Date Registered', 'score': 'Sort by Relevance' });
+      this.set('helpText', 'Blabla');
+    } else if (this.get('model').get("modelName") === "prefix") {
+      this.set('formats', { '-created': 'Sort by Date Created', 'score': 'Sort by Relevance' });
       this.set('helpText', 'Blabla');
     } else {
-      this.set('formats', { '-created': 'Sort by Start Date', 'name': 'Sort by Name' });
+      this.set('formats', { '-created': 'Sort by Date Joined', 'name': 'Sort by Name' });
       this.set('helpText', 'Blabla');
     }
   }
