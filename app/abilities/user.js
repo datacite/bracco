f9bcb9a4ba5046b04393620c68a045153ddeeaab
Martin Fenner <mfenner@datacite.org>
2017-10-08 08:36:21 +0200

    don't allow users to change their role

--- app/abilities/user.js
+++ app/abilities/user.js
@@ -5,7 +5,9 @@ export default Ability.extend({
   canDelete: Ember.computed('currentUser.role_id', 'currentUser.id', 'model.id', function() {
     switch(this.get('currentUser.role_id')) {
       case 'staff_admin':
-        return true;
+      case 'provider_admin':
+      case 'client_admin':
+        return this.get('currentUser.uid') !== this.get('model.id');
       default:
         return false;
     }
@@ -20,14 +22,16 @@ export default Ability.extend({
         return false;
     }
   }),
-  canModify: Ember.computed('currentUser.role_id', 'currentUser.id', 'model.id', function() {
+  canModify: Ember.computed('currentUser.role_id', 'currentUser.id', 'model.id', 'link', function() {
     switch(this.get('currentUser.role_id')) {
       case 'staff_admin':
-        return true;
-        case 'provider_admin':
-          return this.get('currentUser.provider_id') === this.get('model.provider.id');
-        case 'client_admin':
-          return this.get('currentUser.client_id') === this.get('model.client.id');
+        return this.get('link') !== 'clients.show.users' || this.get('currentUser.uid') !== this.get('model.id');
+      case 'provider_admin':
+        return this.get('currentUser.provider_id') === this.get('model.provider.id') &&
+          this.get('link') !== 'clients.show.users' || this.get('currentUser.uid') !== this.get('model.id');
+      case 'client_admin':
+        return this.get('currentUser.client_id') === this.get('model.client.id') &&
+          this.get('link') !== 'clients.show.users' || this.get('currentUser.uid') !== this.get('model.id');
       default:
         return false;
     }
