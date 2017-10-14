c470bcb53a876cab5b23822748de07c5a6d12fc3
Martin Fenner <mfenner@datacite.org>
2017-10-14 20:12:34 +0200

    use sandbox helper

--- app/components/landing-page.js
+++ app/components/landing-page.js
@@ -1,9 +1,4 @@
 import Ember from 'ember';
-import ENV from 'bracco/config/environment';
 
 export default Ember.Component.extend({
-
-  didInsertElement: function() {
-    this.set('isSandbox', ENV.IS_SANDBOX);
-  }
 });
