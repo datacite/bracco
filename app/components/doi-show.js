a6d8cb11c9a032a6aa992189554241c3d7876850
Martin Fenner <mfenner@datacite.org>
2017-10-17 00:24:20 +0200

    edit client (and provider) for a single DOI

--- app/components/doi-show.js
+++ app/components/doi-show.js
@@ -5,14 +5,35 @@ export default Ember.Component.extend({
 
   edit: false,
   doi: null,
+  client: null,
+  clients: [],
+
+  searchClient(query) {
+    if (this.get('currentUser').get('isAdmin')) {
+      this.set('clients', this.get('store').query('client', { 'query': query, sort: 'name', 'page[size]': 25 }));
+    } else if (this.get('currentUser').get('isProvider')) {
+      this.set('clients', this.get('store').query('client', { 'query': query, 'provider-id': this.get('currentUser').get('providerId'), sort: 'name', 'page[size]': 25 }));
+    }
+  },
+  selectClient(client) {
+    this.set('client', client)
+    this.get('doi').set('client', client);
+    this.get('doi').set('provider', client.get('provider'));
+  },
 
   actions: {
     edit: function(doi) {
       this.set('doi', doi);
       this.set('edit', true);
     },
-    submit: function() {
-      //doi.save();
+    searchClient(query) {
+      this.searchClient(query);
+    },
+    selectClient(client) {
+      this.selectClient(client);
+    },
+    submit: function(doi) {
+      doi.save();
       this.set('edit', false);
     },
     cancel: function() {
