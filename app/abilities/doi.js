0cd9005aaf5255890515afff18da3da69a372f5e
Martin Fenner <mfenner@datacite.org>
2017-10-17 22:23:58 +0200

    delete client and provider and transfer doi requires staff_admin role

--- app/abilities/doi.js
+++ app/abilities/doi.js
@@ -2,6 +2,14 @@ import Ember from 'ember';
 import { Ability } from 'ember-can';
 
 export default Ability.extend({
+  canDelete: Ember.computed('currentUser.role_id', function() {
+    switch(this.get('currentUser.role_id')) {
+      case 'staff_admin':
+        return true;
+      default:
+        return false;
+    }
+  }),
   canCreate: Ember.computed('currentUser.role_id', function() {
     switch(this.get('currentUser.role_id')) {
       case 'staff_admin':
