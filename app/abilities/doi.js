ae38d8627629094953a1c46fe40765e4e3ae0f58
Martin Fenner <mfenner@datacite.org>
2017-10-18 15:31:46 +0200

    allow client_admins to create new DOIs

--- app/abilities/doi.js
+++ app/abilities/doi.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import { Ability } from 'ember-can';
 
 export default Ability.extend({
-  canDelete: Ember.computed('currentUser.role_id', function() {
+  canDelete: Ember.computed('currentUser.role_id', 'model.otherParams.client-id', function() {
     switch(this.get('currentUser.role_id')) {
       case 'staff_admin':
         return true;
@@ -10,10 +10,12 @@ export default Ability.extend({
         return false;
     }
   }),
-  canCreate: Ember.computed('currentUser.role_id', function() {
+  canCreate: Ember.computed('currentUser.role_id', 'currentUser.client_id', 'model.otherParams.client-id', function() {
     switch(this.get('currentUser.role_id')) {
       case 'staff_admin':
         return true;
+      case 'client_admin':
+        return this.get('currentUser.client_id') === this.get('model.otherParams.client-id');
       default:
         return false;
     }
