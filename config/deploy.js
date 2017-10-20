ecb1c53bd4e7503e541f67617102e0f8b804542e
Martin Fenner <mfenner@datacite.org>
2017-10-20 18:02:00 +0200

    build app with travis

--- config/deploy.js
+++ config/deploy.js
@@ -2,10 +2,10 @@ module.exports = function(deployTarget) {
   var ENV = {};
 
   ENV.s3 = {
-    accessKeyId: ENV.AWS_ACCESS_KEY,
-    secretAccessKey: ENV.AWS_SECRET_KEY,
-    bucket: ENV.AWS_BUCKET,
-    region: ENV.AWS_REGION,
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
+    bucket: process.env.AWS_BUCKET,
+    region: process.env.AWS_REGION,
     filePattern: '**/*.{js,css,png,gif,ico,jpg,map,xml,txt,svg,swf,eot,ttf,woff,woff2,html}'
   };
 
