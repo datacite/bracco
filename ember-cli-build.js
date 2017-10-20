9800bf39945860b6c06b97c160501a5b010a4361
Martin Fenner <mfenner@datacite.org>
2017-10-20 15:53:35 +0200

    provide env variables for ember deploy

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -29,6 +29,23 @@ module.exports = function(defaults) {
     'ember-prism': {
       'theme': 'default'
     },
+    dotEnv: {
+      clientAllowedKeys: ['SITE_TITLE',
+                          'NAVMENU_TITLE',
+                          'SEARCH_URL',
+                          'ORCID_URL',
+                          'API_URL',
+                          'USER_API_URL',
+                          'DATA_URL',
+                          'CDN_URL',
+                          'JWT_HOST',
+                          'JWT_PUBLIC_KEY',
+                          'BUGSNAG_API_KEY',
+                          'AWS_ACCESS_KEY',
+                          'AWS_SECRET_KEY',
+                          'AWS_BUCKET',
+                          'AWS_REGION']
+    },
     inlineContent: {
       'site-title' : {
         content: (process.env.SITE_TITLE || "DataCite DOI Fabrica")
