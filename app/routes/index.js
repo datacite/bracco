bd2fda15907bf535ea6f2b2abd7f722aeba1b21f
Martin Fenner <mfenner@datacite.org>
2017-11-04 08:53:15 +0000

    handle errors with lupo api

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,9 +3,23 @@ import RouteMixin from 'ember-cli-pagination/remote/route-mixin';
 import { CanMixin } from 'ember-can';
 
 export default Ember.Route.extend(CanMixin, RouteMixin, {
+  flashMessages: Ember.inject.service(),
 
   model() {
-    return this.store.findRecord('provider', 'admin');
+    let self = this;
+    this.store.findRecord('provider', 'admin').then(function(admin) {
+      return admin;
+    }).catch(function(reason){
+      let error = reason.errors[0];
+      console.assert(false, error);
+
+      if (error.title === "Adapter Error") {
+        self.get('flashMessages').warning('There is a problem with the DataCite API. Please try again later or contact DataCite Support.', {
+          timeout: 5000,
+          sticky: true
+        });
+      }
+    });
   },
 
   afterModel() {
