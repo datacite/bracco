3f57bc729c2c5db24544cc4a78638ae77ec3733a
Martin Fenner <mfenner@datacite.org>
2017-11-06 09:13:30 +0100

    availableUsers is no longer needed

--- app/components/user-list.js
+++ app/components/user-list.js
@@ -4,20 +4,5 @@ export default Ember.Component.extend({
   store: Ember.inject.service(),
 
   tagName: 'div',
-  classNames: ['row'],
-  availableUsers: [],
-
-  didInsertElement: function() {
-    if (this.get('model.otherParams.sandbox-id')) {
-      this.set('availableUsers', this.get('store').query('user', { sandbox: true, exclude: true, sort: 'name', 'page[size]': 25 }));
-    } else if (this.get('model.otherParams.provider-id') === 'sandbox') {
-      this.set('availableUsers', []);
-    } else if (this.get('model.otherParams.client-id')) {
-      this.set('availableUsers', this.get('store').query('user', { 'client-id': this.get('model.otherParams.client-id'), exclude: true, sort: 'name', 'page[size]': 25 }));
-    } else if (this.get('model.otherParams.provider-id')) {
-      this.set('availableUsers', this.get('store').query('user', { 'provider-id': this.get('model.otherParams.provider-id'), exclude: true, sort: 'name', 'page[size]': 25 }));
-    } else {
-      this.set('availableUsers', this.get('store').query('user', { sort: 'name', 'page[size]': 10 }));
-    }
-  }
+  classNames: ['row']
 });
