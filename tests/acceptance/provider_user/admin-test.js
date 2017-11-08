b15e077d76b1c0e41554d34b2f550c5f35865f7f
Martin Fenner <mfenner@datacite.org>
2017-11-08 11:37:13 +0100

    fixed acceptance tests

--- tests/acceptance/provider_user/admin-test.js
+++ tests/acceptance/provider_user/admin-test.js
@@ -120,7 +120,7 @@ test('visiting specific doi not managed by provider', function(assert) {
 });
 
 test('visiting specific doi managed by provider', function(assert) {
-  visit('/dois/10.2312/cr_m84_4');
+  visit('/dois/10.2312%2Fcr_m84_4');
 
   andThen(function() {
     assert.equal(currentURL(), '/dois/10.2312%2Fcr_m84_4');
