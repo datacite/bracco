22c004015cd68a7dbbef7c9d59da465e06b469ab
Martin Fenner <mfenner@datacite.org>
2017-12-11 22:04:46 +0100

    sign_out via ajax

--- app/components/menu-user.js
+++ app/components/menu-user.js
@@ -1,5 +1,7 @@
 import Ember from 'ember';
 import ENV from 'bracco/config/environment';
+//import fetch from 'fetch';
+//import Cookie from 'ember-cli-js-cookie';
 
 export default Ember.Component.extend({
   tagName: 'li',
@@ -7,6 +9,20 @@ export default Ember.Component.extend({
   dropdownMenu: Ember.computed(function() {
     this.get('currentUser').get('isAuthenticated');
   }),
+
+  // actions: {
+  //   signout() {
+  //     let url = ENV.JWT_HOST + '/sign_out';
+  //     let self = this;
+  //     fetch(url).then(function(response) {
+  //       if (response.ok) {
+  //         //Cookie.remove('_datacite_jwt');
+  //         //self.get('currentUser').set('role_id', null);
+  //         //self.get('router').transitionTo('index');
+  //       }
+  //     });
+  //   }
+  // },
 
   didInsertElement: function() {
     this.set('currentUser', this.get('currentUser'));
