34b182e6137cbb843cbe39b1ded7ed95c5ed39ae
Martin Fenner <mfenner@datacite.org>
2017-12-12 01:25:16 +0100

    added tests

--- tests/unit/abilities/client-test.js
+++ tests/unit/abilities/client-test.js
@@ -1,12 +1,12 @@
-// import Ember from 'ember';
-// import { moduleFor, test } from 'ember-qunit';
-//
-// moduleFor('ability:client', 'Unit | Ability | client', {
-//   currentUser: Ember.inject.service()
-// });
-//
-// // Replace this with your real tests.
-// test('it exists', function(assert) {
-//   var ability = this.subject();
-//   assert.ok(ability);
-// });
+import Ember from 'ember';
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('ability:client', 'Unit | Ability | client', {
+  currentUser: Ember.inject.service()
+});
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  var ability = this.subject();
+  assert.ok(ability);
+});
--- tests/unit/abilities/user-test.js
+++ tests/unit/abilities/user-test.js
@@ -1,12 +1,12 @@
-// import Ember from 'ember';
-// import { moduleFor, test } from 'ember-qunit';
-//
-// moduleFor('ability:user', 'Unit | Ability | user', {
-//   currentUser: Ember.inject.service()
-// });
-//
-// // Replace this with your real tests.
-// test('it exists', function(assert) {
-//   var ability = this.subject();
-//   assert.ok(ability);
-// });
+import Ember from 'ember';
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('ability:user', 'Unit | Ability | user', {
+  currentUser: Ember.inject.service()
+});
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  var ability = this.subject();
+  assert.ok(ability);
+});
