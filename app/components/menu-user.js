aaa3f94d12041658f4ac34c77acd1d878cd29a0f
Martin Fenner <mfenner@datacite.org>
2018-01-28 21:01:50 +0100

    fix reset password function

--- app/components/menu-user.js
+++ app/components/menu-user.js
@@ -1,7 +1,4 @@
 import Ember from 'ember';
-import ENV from 'bracco/config/environment';
-//import fetch from 'fetch';
-//import Cookie from 'ember-cli-js-cookie';
 
 export default Ember.Component.extend({
   session: Ember.inject.service('session'),
@@ -12,29 +9,9 @@ export default Ember.Component.extend({
     this.get('session').get('isAuthenticated');
   }),
 
-  // actions: {
-  //   signout() {
-  //     let url = ENV.JWT_HOST + '/sign_out';
-  //     let self = this;
-  //     fetch(url).then(function(response) {
-  //       if (response.ok) {
-  //         //Cookie.remove('_datacite_jwt');
-  //         //self.get('currentUser').set('role_id', null);
-  //         //self.get('router').transitionTo('index');
-  //       }
-  //     });
-  //   }
-  // },
-
   actions: {
     invalidateSession() {
       this.get('session').invalidate();
     }
-  },
-
-  didInsertElement: function() {
-    this.set('currentUser', this.get('currentUser'));
-    this.set('signout', { href: ENV.JWT_HOST + '/sign_out' })
-    this.set('signin', { href: ENV.JWT_HOST + '/sign_in', className: 'btn btn-sm btn-neutral' })
   }
 });
