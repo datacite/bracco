b833ea3ac656d2f9079ab69ddaeb073179bee476
Martin Fenner <mfenner@datacite.org>
2018-01-29 21:49:19 +0100

    add missing ability

--- app/abilities/client.js
+++ app/abilities/client.js
@@ -4,7 +4,7 @@ import { Ability } from 'ember-can';
 
 export default Ability.extend({
   currentUser: service(),
-  
+
   canDelete: Ember.computed('currentUser.role_id', function() {
     switch(this.get('currentUser.role_id')) {
       case 'staff_admin':
@@ -44,8 +44,9 @@ export default Ability.extend({
       case 'provider_admin':
         return this.get('currentUser.provider_id') === (this.get('model.id') && this.get('model.id').split('.').get('firstObject'));
       case 'client_admin':
+        return this.get('currentUser.client_id') === this.get('model.id');
       default:
-        return this.get('currentUser.sandbox_id') === this.get('model.id');
+        return false
     }
   })
 });
