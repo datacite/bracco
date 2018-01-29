73391b0496bd3d229789526d403fec3705755946
Martin Fenner <mfenner@datacite.org>
2018-01-29 12:03:44 +0100

    configure ember-deploy for s3

--- config/deploy.js
+++ config/deploy.js
@@ -6,5 +6,13 @@ module.exports = function(deployTarget) {
     publicUrl: process.env.BUGSNAG_PUBLIC_URL,
   };
 
+  ENV.s3 = {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
+    bucket: process.env.AWS_BUCKET,
+    region: process.env.AWS_REGION,
+    filePattern: '**/*.{js,css,png,gif,ico,jpg,map,xml,txt,svg,swf,eot,ttf,woff,woff2,html}'
+  };
+
   return ENV;
 };
