30468d917c0143b14240d2b23cf2d2d43016699b
Martin Fenner <mfenner@datacite.org>
2018-01-29 12:16:58 +0100

    use ember-cli-deploy-aws-pack

--- config/deploy.js
+++ config/deploy.js
@@ -1,18 +1,42 @@
 module.exports = function(deployTarget) {
-  var ENV = {};
-
-  ENV['bugsnag'] = {
-    apiKey: process.env.BUGSNAG_API_KEY,
-    publicUrl: process.env.BUGSNAG_PUBLIC_URL,
+  let ENV = {
+    build: {},
+    pipeline: {
+      // This setting runs the ember-cli-deploy activation hooks on every deploy
+      // which is necessary in order to run ember-cli-deploy-cloudfront.
+      // To disable CloudFront invalidation, remove this setting or change it to `false`.
+      // To disable ember-cli-deploy-cloudfront for only a particular environment, add
+      // `ENV.pipeline.activateOnDeploy = false` to an environment conditional below.
+      activateOnDeploy: true
+    },
+    s3: {
+      accessKeyId: process.env.AWS_ACCESS_KEY,
+      secretAccessKey: process.env.AWS_SECRET_KEY,
+      filePattern: '*'
+    },
+    cloudfront: {
+      accessKeyId: process.env.AWS_ACCESS_KEY,
+      secretAccessKey: process.env.AWS_SECRET_KEY
+    },
+    bugsnag: {
+      apiKey: process.env.BUGSNAG_API_KEY,
+      publicUrl: process.env.BUGSNAG_PUBLIC_URL,
+    }
   };
 
-  ENV.s3 = {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_KEY,
-    bucket: process.env.AWS_BUCKET,
-    region: process.env.AWS_REGION,
-    filePattern: '**/*.{js,css,png,gif,ico,jpg,map,xml,txt,svg,swf,eot,ttf,woff,woff2,html}'
-  };
+  if (deployTarget === 'staging') {
+    ENV.build.environment = 'production';
+    ENV.s3.bucket = process.env.STAGING_BUCKET;
+    ENV.s3.region = process.env.STAGING_REGION;
+    ENV.cloudfront.distribution = process.env.STAGING_DISTRIBUTION;
+  }
+
+  if (deployTarget === 'production') {
+    ENV.build.environment = 'production';
+    ENV.s3.bucket = process.env.PRODUCTION_BUCKET;
+    ENV.s3.region = process.env.PRODUCTION_REGION;
+    ENV.cloudfront.distribution = process.env.PRODUCTION_DISTRIBUTION;
+  }
 
   return ENV;
 };
