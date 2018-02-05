15703911f1f0a61a49b90281dcfeaa7fe53ea21e
Martin Fenner <mfenner@datacite.org>
2018-02-05 09:18:35 +0100

    fix signin workflow via token

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,8 +3,11 @@ import { inject as service } from '@ember/service';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
+  session: service(),
   currentUser: service(),
   intl: service(),
+
+  isTokenAuthenticating: null,
 
   beforeModel() {
     // if you lazily load translations, here is also where you would load them via `intl.addTranslations`
@@ -13,9 +16,33 @@ export default Ember.Route.extend(ApplicationRouteMixin, {
     return this._loadCurrentUser();
   },
 
+  model(params) {
+    const { jwt } = params;
+    if (jwt) {
+      this.set('isTokenAuthenticating', true);
+      if (this.get('session.isAuthenticated')) {
+        this.get('session').invalidate();
+      }
+      let self = this;
+      this.get('session').authenticate('authenticator:jwt', jwt).then(function() {
+        return self._loadCurrentUser();
+      });
+    }
+  },
+
   sessionAuthenticated() {
-    this._super(...arguments);
-    this._loadCurrentUser();
+    if (!this.get('isTokenAuthenticating')) {
+      this._super(...arguments);
+      this._loadCurrentUser();
+    } else {
+      this.set('isTokenAuthenticating', false);
+    }
+  },
+
+  sessionInvalidated() {
+    if (!this.get('isTokenAuthenticating')) {
+      this._super();
+    }
   },
 
   _loadCurrentUser() {
