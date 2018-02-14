e96fad59a5b49b6f0341ce5b99d249568c66cbf8
Martin Fenner <mfenner@datacite.org>
2018-02-14 22:45:58 +0100

    adjust state label colors

--- app/components/state-label.js
+++ app/components/state-label.js
@@ -6,10 +6,10 @@ export default Component.extend({
 
   didInsertElement() {
     let state = this.get('state') || 'initial';
-    let stateLabels = { 'new': 'label-warning',
-                       'draft': 'label-default',
-                       'registered': 'label-info',
-                       'findable': 'label-primary' }
+    let stateLabels = { 'inactive': 'label-default',
+                        'draft': 'label-warning',
+                        'registered': 'label-info',
+                        'findable': 'label-primary' }
 
     this.set('label', 'label ' + stateLabels[state]);
     this.set('stateDisplay', state.capitalize());
