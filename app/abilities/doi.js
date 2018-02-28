f545550b6f9fcd0ea7373c28dd696afbf97f8e6e
Martin Fenner <mfenner@datacite.org>
2018-02-28 20:06:59 +0100

    fixed client permissions to edit dois

--- app/abilities/doi.js
+++ app/abilities/doi.js
@@ -47,14 +47,14 @@ export default Ability.extend({
         return false;
     }
   }),
-  canEdit: Ember.computed('currentUser.role_id', 'model.provider.id', 'model.id', function() {
+  canEdit: Ember.computed('currentUser.role_id', 'model.provider.id', 'model.client.id', 'model.id', function() {
     switch(this.get('currentUser.role_id')) {
       case 'staff_admin':
         return true;
       case 'provider_admin':
         return this.get('currentUser.provider_id') === this.get('model.provider.id');
       case 'client_admin':
-        return this.get('currentUser.client_id') === this.get('model.id');
+        return this.get('currentUser.client_id') === this.get('model.client.id');
       default:
         return false;
     }
