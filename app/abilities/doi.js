1316ddf5ccd1dfd6bee2de0ccb63cb6af541987e
Martin Fenner <mfenner@datacite.org>
2018-03-15 13:14:53 +0100

    support doi management for test provider in production

--- app/abilities/doi.js
+++ app/abilities/doi.js
@@ -30,7 +30,7 @@ export default Ability.extend({
   canCreate: Ember.computed('currentUser.role_id', 'model.otherParams.client-id', function() {
     switch(this.get('currentUser.role_id')) {
       case 'client_admin':
-        return ENV.APP_URL !== "https://app.datacite.org" && this.get('currentUser.client_id') === this.get('model.otherParams.client-id');
+        return (ENV.APP_URL !== "https://app.datacite.org" || this.get('currentUser.provider_id') === 'test') && this.get('currentUser.client_id') === this.get('model.otherParams.client-id');
       default:
         return false;
     }
@@ -38,7 +38,7 @@ export default Ability.extend({
   canDelete: Ember.computed('currentUser.role_id', 'model.client.id', function() {
     switch(this.get('currentUser.role_id')) {
       case 'client_admin':
-        return ENV.APP_URL !== "https://app.datacite.org" && this.get('currentUser.client_id') === this.get('model.client.id');
+        return (ENV.APP_URL !== "https://app.datacite.org" || this.get('currentUser.provider_id') === 'test') && this.get('currentUser.client_id') === this.get('model.client.id');
       default:
         return false;
     }
@@ -46,7 +46,7 @@ export default Ability.extend({
   canEdit: Ember.computed('currentUser.role_id', 'model.client.id', function() {
     switch(this.get('currentUser.role_id')) {
       case 'client_admin':
-        return ENV.APP_URL !== "https://app.datacite.org" && this.get('currentUser.client_id') === this.get('model.client.id');
+        return (ENV.APP_URL !== "https://app.datacite.org" || this.get('currentUser.provider_id') === 'test') && this.get('currentUser.client_id') === this.get('model.client.id');
       default:
         return false;
     }
