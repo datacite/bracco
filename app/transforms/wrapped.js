585eb53143f4536375e2ea20b7295b5a05631c5f
Martin Fenner <martin.fenner@datacite.org>
2018-05-19 13:40:20 +0200

    handle multiple titles and descriptions. #83

--- app/transforms/wrapped.js
+++ app/transforms/wrapped.js
@@ -4,7 +4,7 @@ import Ember from 'ember';
 export default DS.Transform.extend({
   deserialize(serialized) {
     if (Ember.typeOf(serialized) === 'array') {
-      this.deserialize(serialized[0]);
+      return this.deserialize(serialized[0]);
     } else if (Ember.typeOf(serialized) === 'object') {
       return serialized.text;
     } else {
