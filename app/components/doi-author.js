d29f62f2433fcdb6d8b3adbaabb029e2fe0e06cf
Martin Fenner <martin.fenner@datacite.org>
2018-05-20 19:38:43 +0200

    leave empty space in author field #80

--- app/components/doi-author.js
+++ app/components/doi-author.js
@@ -4,12 +4,10 @@ export default Component.extend({
   didInsertElement() {
     let authorNumber = this.get('model').get('author').split("\n").length;
 
-    if (authorNumber == 1) {
-      this.set('authorNumber', 5);
-    } else if (authorNumber > 25) { 
+    if (authorNumber > 24) { 
       this.set('authorNumber', 25);
     } else {
-      this.set('authorNumber', authorNumber);
+      this.set('authorNumber', authorNumber + 1);
     }
   }
 });
