96f97e181d62b4e75c194ca9f13f967bfc56ac9b
Martin Fenner <martin.fenner@datacite.org>
2018-07-04 20:24:08 +0200

    handle xml parsing errors

--- app/transforms/xml.js
+++ app/transforms/xml.js
@@ -27,9 +27,14 @@ export default DS.Transform.extend({
       }));
   },
   b64DecodeUnicode(str) {
-    // Going backwards: from bytestream, to percent-encoding, to original string.
-    return decodeURIComponent(atob(str).split('').map(function(c) {
+    try {
+      // Going backwards: from bytestream, to percent-encoding, to original string.
+      return decodeURIComponent(atob(str).split('').map(function(c) {
         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
+      }).join(''));
+    }
+    catch(err) {
+      return null;
+    }
   }
 });
