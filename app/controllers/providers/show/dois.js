6a7f58b0658699fbd51f25030a6252262afd6d24
Martin Fenner <martin.fenner@datacite.org>
2018-08-05 00:34:39 +0200

    doi queries by orcid.

--- app/controllers/providers/show/dois.js
+++ app/controllers/providers/show/dois.js
@@ -1,11 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  queryParams: ['query', 'resource-type-id', 'provider-id', 'client-id', 'prefix', 'year', 'created', 'state', 'sort', 'source', 'schema-version', 'page', 'perPage'],
+  queryParams: ['query', 'resource-type-id', 'provider-id', 'client-id', 'person-id', 'prefix', 'year', 'created', 'state', 'sort', 'source', 'schema-version', 'page', 'perPage'],
   query: null,
   'resource-type-id': null,
   'provider-id': null,
   'client-id': null,
+  'person-id': null,
   prefix: null,
   year: null,
   created: null,
