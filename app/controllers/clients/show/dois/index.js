8daa661f5492725cfac2c0da4b6844c15d38d210
Martin Fenner <martin.fenner@datacite.org>
2018-08-05 00:00:26 +0200

    use year created as facet

--- app/controllers/clients/show/dois/index.js
+++ app/controllers/clients/show/dois/index.js
@@ -1,14 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  queryParams: ['query', 'resource-type-id', 'provider-id', 'client-id', 'prefix', 'year', 'state', 'sort', 'registered', 'schema-version', 'source', 'page', 'perPage'],
+  queryParams: ['query', 'resource-type-id', 'provider-id', 'client-id', 'prefix', 'year', 'state', 'sort', 'created', 'schema-version', 'source', 'page', 'perPage'],
   query: null,
   'resource-type-id': null,
   'provider-id': null,
   'client-id': null,
   prefix: null,
   year: null,
-  registered: null,
+  created: null,
   state: null,
   source: null,
   sort: null,
