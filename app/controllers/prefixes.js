f3b012a40de5e84880ba69efae3259d670fd054d
Martin Fenner <martin.fenner@datacite.org>
2018-08-05 18:18:42 +0200

    link to dois associated with prefix. #92

--- app/controllers/prefixes.js
+++ app/controllers/prefixes.js
@@ -1,4 +1,6 @@
 import Ember from 'ember';
+import { inject as service } from '@ember/service';
 
 export default Ember.Controller.extend({
+  features: service()
 });
--- app/controllers/prefixes/index.js
+++ app/controllers/prefixes/index.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
+import { inject as service } from '@ember/service';
 
 export default Ember.Controller.extend({
+  features: service(), 
+  
   queryParams: ['query', 'provider-id', 'client-id', 'year', 'state', 'sort', 'page', 'perPage'],
   query: null,
   'provider-id': null,
