08c9adc4e2e2167dc8ca97e07c9ec2df5fae93db
Martin Fenner <martin.fenner@datacite.org>
2018-08-31 22:43:23 +0200

    show url from handle system. datacite/bracco#104

--- app/components/doi-url.js
+++ app/components/doi-url.js
@@ -1,4 +1,43 @@
 import Ember from 'ember';
+import fetch from 'fetch';
+const { service } = Ember.inject;
+import ENV from 'bracco/config/environment';
 
 export default Ember.Component.extend({
+  currentUser: service(),
+
+  url: null,
+
+  didReceiveAttrs() {
+    this._super(...arguments);
+
+    this.fetchURL();
+  },
+
+  fetchURL() {
+    let self = this;
+    let url = ENV.API_URL + '/dois/' + this.get('model').get('doi') + '/get-url';
+
+    fetch(url, {
+      headers: {
+        'Authorization': 'Bearer ' + self.get('currentUser').get('jwt'),
+        'Accept': 'application/vnd.api+json'
+      }
+    }).then(function(response) {
+      if (response.ok) {
+        return response.json().then(function(data) {
+          if (data.errors) {
+            let message = data.errors[0].title;
+            return message;
+          } else {
+            self.set('url', data.url);
+          }
+        });
+      } else {
+        Ember.Logger.assert(false, response);
+      }
+    }).catch(function(error) {
+      Ember.Logger.assert(false, error);
+    });
+  }
 });
