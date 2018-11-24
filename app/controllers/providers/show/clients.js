3c473d1c4a1b4d52a986ccf9587a59c8b2f0fe55
Martin Fenner <martin.fenner@datacite.org>
2018-11-24 18:37:47 +0100

    allow provider queries by software

--- app/controllers/providers/show/clients.js
+++ app/controllers/providers/show/clients.js
@@ -1,9 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  queryParams: ['query', 'year', 'provider-id', 'sort', 'page', 'perPage'],
+  queryParams: ['query', 'year', 'provider-id', 'software', 'sort', 'page', 'perPage'],
   query: null,
   year: null,
   'provider-id': null,
+  software: null,
   sort: null
 });
