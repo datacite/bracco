648c793c0741a30cf418a893c162113e6a1811bb
Martin Fenner <martin.fenner@datacite.org>
2018-11-24 14:35:15 +0100

    show client software facet

--- app/controllers/clients/index.js
+++ app/controllers/clients/index.js
@@ -1,10 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  queryParams: ['query', 'year', 'provider-id', 'include-deleted', 'sort', 'page', 'perPage'],
+  queryParams: ['query', 'year', 'provider-id', 'software', 'include-deleted', 'sort', 'page', 'perPage'],
   query: null,
   year: null,
   'provider-id': null,
+  software: null,
   'include-deleted': null,
   sort: null
 });
