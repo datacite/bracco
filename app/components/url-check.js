88fe7e3a9035b44bc16acb4de44c7fbd71c208e7
Martin Fenner <martin.fenner@datacite.org>
2018-12-23 18:01:56 +0100

    don't run link checker

--- app/components/url-check.js
+++ app/components/url-check.js
@@ -6,45 +6,6 @@ import ENV from 'bracco/config/environment';
 export default Ember.Component.extend({
   currentUser: service(),
 
-  init() {
-    this._super();
-    Ember.run.schedule("afterRender",this,function() {
-      this.send("checkLink");
-    });
-  },
-
-  actions: {
-    checkLink() {
-      let self = this;
-      let url = ENV.API_URL + '/dois/status?id=' + this.get('model').get('doi');
-      fetch(url, {
-        method: 'post',
-        timeout: 5000,
-        headers: {
-          'Authorization': 'Bearer ' + this.get('currentUser').get('jwt')
-        }
-      }).then(function(response) {
-        if (response.ok) {
-          response.json().then(function(result) {
-            if (parseInt(result.status) === 403) {
-              self.set('message', "Access to page was forbidden (status code " + result.status + ").");
-            } else if (result.status === 404) {
-              self.set('message', "Page was not found (status code " + result.status + ").");
-            } else if (result.status === 408) {
-              self.set('message', "Request timed out reaching the page (status code " + result.status + ").");
-            } else if (result['content-type'] !== "text/html") {
-              self.set('message', "Page should be a web page, but was content type " + result['content-type'] + ".");
-            }
-          });
-        } else {
-          Ember.Logger.assert(false, response)
-        }
-      }).catch(function(error) {
-        Ember.Logger.assert(false, error)
-      });
-    }
-  },
-
   didInsertElement() {
     this.set('url', this.get('url'));
   }
