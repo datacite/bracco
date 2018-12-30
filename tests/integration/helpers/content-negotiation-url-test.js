eb526e96f7b3b393e312cdf5d6e909c6b3e4aba3
Martin Fenner <martin.fenner@datacite.org>
2018-12-30 13:11:48 +0100

    fixed helper tests. #118

--- tests/integration/helpers/content-negotiation-url-test.js
+++ tests/integration/helpers/content-negotiation-url-test.js
@@ -1,8 +1,9 @@
 
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import ENV from 'bracco/config/environment';
 
-moduleForComponent('content-negotiation', 'helper:content-negotiation', {
+moduleForComponent('content-negotiation-url', 'helper:content-negotiation-url', {
   integration: true
 });
 
@@ -10,8 +11,8 @@ moduleForComponent('content-negotiation', 'helper:content-negotiation', {
 test('it renders', function(assert) {
   this.set('inputValue', '1234');
 
-  this.render(hbs`{{content-negotiation inputValue}}`);
+  this.render(hbs`{{content-negotiation-url inputValue}}`);
 
-  assert.equal(this.$().text().trim(), '1234');
+  assert.equal(this.$().text().trim(), ENV.API_URL + '/dois/application/vnd.schemaorg.ld+json/1234');
 });
 
--- tests/integration/helpers/format-metadata-test.js
+++ tests/integration/helpers/format-metadata-test.js
@@ -11,5 +11,5 @@ test('it renders', function(assert) {
 
   this.render(hbs`{{format-metadata inputValue}}`);
 
-  assert.equal(this.$().text().trim(), 'Work published 1234');
+  assert.equal(this.$().text().trim(), 'published 1234');
 });
