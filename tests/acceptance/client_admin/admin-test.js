bd0f21d39ded109f9286b405571cdfae8babfb28
Martin Fenner <martin.fenner@datacite.org>
2018-12-31 07:49:22 +0100

    update acceptance tests for ember-simple-auth

--- tests/acceptance/client_admin/admin-test.js
+++ tests/acceptance/client_admin/admin-test.js
@@ -1,3 +1,79 @@
+import { module, test } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
+import { currentURL, visit } from '@ember/test-helpers';
+import { authenticateSession } from 'ember-simple-auth/test-support';
+
+module('Acceptance | client_admin | admin', function(hooks) {
+  setupApplicationTest(hooks);
+
+  test('visiting homepage', async function(assert) {
+    await authenticateSession({
+      uid: 'tib.awi',
+      name: 'Alfred Wegener Institute',
+      role_id: 'client_admin',
+      provider_id: 'tib',
+      client_id: 'tib.awi'
+    });
+    await visit('/');
+    assert.equal(currentURL(), '/');
+    assert.equal(find('div.motto h1').text(), 'DataCite DOI Fabrica');
+  });
+
+  // the following pages require authentication. Redirects to homepage otherwise
+  test('visiting settings', async function(assert) {
+    await authenticateSession({
+      uid: 'tib.awi',
+      name: 'Alfred Wegener Institute',
+      role_id: 'client_admin',
+      provider_id: 'tib',
+      client_id: 'tib.awi'
+    });
+    await visit('/settings');
+    assert.equal(currentURL(), '/');
+    assert.equal(find('div.motto h1').text(), 'DataCite DOI Fabrica');
+  });
+
+  test('visiting providers', async function(assert) {
+    await authenticateSession({
+      uid: 'tib.awi',
+      name: 'Alfred Wegener Institute',
+      role_id: 'client_admin',
+      provider_id: 'tib',
+      client_id: 'tib.awi'
+    });
+    await visit('/providers');
+    assert.equal(currentURL(), '/');
+    assert.equal(find('div.motto h1').text(), 'DataCite DOI Fabrica');
+  });
+
+  test('visiting clients', async function(assert) {
+    await authenticateSession({
+      uid: 'tib.awi',
+      name: 'Alfred Wegener Institute',
+      role_id: 'client_admin',
+      provider_id: 'tib',
+      client_id: 'tib.awi'
+    });
+    await visit('/clients');
+    assert.equal(currentURL(), '/');
+    assert.equal(find('div.motto h1').text(), 'DataCite DOI Fabrica');
+  });
+
+  test('visiting prefixes', async function(assert) {
+    await authenticateSession({
+      uid: 'tib.awi',
+      name: 'Alfred Wegener Institute',
+      role_id: 'client_admin',
+      provider_id: 'tib',
+      client_id: 'tib.awi'
+    });
+    await visit('/prefixes');
+    assert.equal(currentURL(), '/');
+    assert.equal(find('div.motto h1').text(), 'DataCite DOI Fabrica');
+  });
+});
+
+
 // import Ember from 'ember';
 // import { test } from 'qunit';
 // import moduleForAcceptance from 'bracco/tests/helpers/module-for-acceptance';
