8ab76d25c27b32dacf9c9a08fd65d9a62d007be7
Martin Fenner <martin.fenner@datacite.org>
2019-01-09 13:28:27 +0100

    fix client pagination for providers. #131

--- app/controllers/providers/show/clients.js
+++ app/controllers/providers/show/clients.js
@@ -1,10 +1,9 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-  queryParams: ['query', 'year', 'provider-id', 'software', 'sort', 'page', 'size'],
+  queryParams: ['query', 'year', 'software', 'sort', 'page', 'size'],
   query: null,
   year: null,
-  'provider-id': null,
   software: null,
   sort: null,
   page: 1,
