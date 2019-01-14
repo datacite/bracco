2b75e0762746d55035a2e642a86226c19708a79d
Richard Hallett <richard@richardhallett.com>
2019-01-14 18:13:55 +0100

    Only convert back to an array if we're not already an array

--- app/components/doi-titles.js
+++ app/components/doi-titles.js
@@ -5,13 +5,8 @@ export default Component.extend({
   didReceiveAttrs() {
     this._super(...arguments);
 
-    if (isPresent(this.model.get('titles'))) {
-      var titles = this.model.get('titles');
-      if (typeof (titles) === 'string') {
-        this.model.set('titles', titles);
-      } else {
-        this.model.set('titles', titles[0].title);
-      }
+    if (isPresent(this.model.get('titles')) && this.model.get('titles').constructor === Array) {
+      this.model.set('titles', this.model.get('titles')[0].title);
     }
   }
 });
