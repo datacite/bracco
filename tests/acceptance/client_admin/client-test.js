016595a1fab1068e43427498c341ccdda70f2a0d
Martin Fenner <martin.fenner@datacite.org>
2019-03-02 00:27:39 +0100

    fix test. #160

--- tests/acceptance/client_admin/client-test.js
+++ tests/acceptance/client_admin/client-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
-import { currentURL, visit, fillIn } from '@ember/test-helpers';
+import { currentURL, visit } from '@ember/test-helpers';
 import { authenticateSession } from 'ember-simple-auth/test-support';
 
 module('Acceptance | client_admin | client', function(hooks) {
