1042817dde69021562ed9b16ab7be2675bddb7a4
Kristian Garza <kj.garza@gmail.com>
2019-03-04 09:27:07 +0100

    test for new form

--- tests/acceptance/client_admin/client-test.js
+++ tests/acceptance/client_admin/client-test.js
@@ -1,7 +1,8 @@
 import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
-import { currentURL, visit, fillIn, click} from '@ember/test-helpers';
+import { currentURL, visit, fillIn, click, pauseTest} from '@ember/test-helpers';
 import { authenticateSession } from 'ember-simple-auth/test-support';
+import { selectChoose, selectSearch, removeMultipleOption, clearSelected } from 'ember-power-select/test-support';
 
 module('Acceptance | client_admin | client', function(hooks) {
   setupApplicationTest(hooks);
@@ -92,7 +93,7 @@ module('Acceptance | client_admin | client', function(hooks) {
     assert.dom('input#suffix-field').hasAnyValue();
   });
 
-  test('creating a new DOI for client AWI', async function(assert) {
+  test('adding fields for a new DOI for client AWI', async function(assert) {
     assert.expect(5);
     await authenticateSession({
       uid: 'tib.awi',
@@ -111,5 +112,31 @@ module('Acceptance | client_admin | client', function(hooks) {
     assert.dom('input#publication-year-field').hasValue("1928");
     assert.dom('input#url-field').hasStyle({color:'rgb(46, 204, 113)'});
     assert.dom('input#publisher-field').hasStyle({color:'rgb(46, 204, 113)'});
+  });
+
+  test('creating a new DOI for client AWI', async function(assert) {
+    assert.expect(3);
+    await authenticateSession({
+      uid: 'tib.awi',
+      name: 'Alfred Wegener Institute',
+      role_id: 'client_admin',
+      provider_id: 'tib',
+      client_id: 'tib.awi'
+    });
+    await visit('/clients/tib.awi/dois/new');
+    await fillIn('input#suffix-field', '111hwks-de38sssssss');
+    await fillIn('input#url-field', 'http://bbc.co.uk');
+    await fillIn('input#title-field-1', 'Abhinandan: Crowds gather for Indian pilots release');
+    await fillIn('input#publisher-field', 'the BBC');
+    await fillIn('input#publication-year-field', 1928);
+    await fillIn('textarea#creator-field', 'Alexander Payne');
+    // await selectChoose('.ember-power-select-trigger', '.ember-power-select-option', 1); 
+    await click('button#create');
+    await pauseTest();
+
+    assert.equal(currentURL(), '/clients/tib.awi/dois/10.2312/111hwks-de38');
+    assert.dom('h2.work').hasValue('10.2312/hwks-de38');
+    assert.dom('h3.work').hasValue('Abhinandan: Crowds gather for Indian pilots release');
+
   });
 });
