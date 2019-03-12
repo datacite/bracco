47c9a1156f70e1aa51b455b1e03d8c8387d6b370
Kristian Garza <kj.garza@gmail.com>
2019-03-12 10:26:03 +0100

    titltes tests

--- tests/integration/components/doi-title-test.js
+++ tests/integration/components/doi-title-test.js
@@ -1,26 +1,16 @@
 // import { module, test } from 'qunit';
 // import { setupRenderingTest } from 'ember-qunit';
-// import { render } from '@ember/test-helpers';
+// import { render,  fillIn, pauseTest } from '@ember/test-helpers';
 // import hbs from 'htmlbars-inline-precompile';
+// import { setupFactoryGuy, make } from 'ember-data-factory-guy';
 
 // module('Integration | Component | doi-title', function(hooks) {
 //   setupRenderingTest(hooks);
 
 //   test('it renders', async function(assert) {
-//     // Set any properties with this.set('myProperty', 'value');
-//     // Handle any actions with this.set('myAction', function(val) { ... });
-
 //     await render(hbs`{{doi-title}}`);
-
-//     assert.equal(this.element.textContent.trim(), '');
-
-//     // Template block usage:
-//     await render(hbs`
-//       {{#doi-title}}
-//         template block text
-//       {{/doi-title}}
-//     `);
-
-//     assert.equal(this.element.textContent.trim(), 'template block text');
+//     await fillIn('input.title-field', "Abhinandan: Crowds gather for Indian pilots release");
+//     await pauseTest();
+//     assert.dom('input.title-field').hasValue("Abhinandan: Crowds gather for Indian pilots release");
 //   });
 // });
