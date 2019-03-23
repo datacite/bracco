b7d21b0b2e177c044e2273cab8edddffaed3b6cd
Martin Fenner <martin.fenner@datacite.org>
2019-03-23 06:29:52 +0100

    send release info to sentry. datacite/datacite#703

--- app/app.js
+++ app/app.js
@@ -5,7 +5,12 @@ import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 import * as Sentry from '@sentry/browser';
 
-Sentry.init({ dsn: config.SENTRY_DSN });
+const pkg = require('./package.json');
+
+Sentry.init({ 
+  dsn: config.SENTRY_DSN,
+  release: pkg.name + ':' + pkg.version
+});
 
 const App = Application.extend({
   modulePrefix: config.modulePrefix,
