1f0fbeafc9fab992e0d53916295e196faf6789de
Martin Fenner <martin.fenner@datacite.org>
2019-04-22 09:55:00 +0200

    set correct class with validations. #192

--- app/components/doi-titles.js
+++ app/components/doi-titles.js
@@ -1,6 +1,8 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  validationsClass: null,
+
   didReceiveAttrs() {
     this._super(...arguments);
 
@@ -12,6 +14,15 @@ export default Component.extend({
   actions: {
     addTitle() {
       this.model.get('titles').createFragment();
+    },
+    setValidationClass() {
+      if (this.model.get('validations.attrs.titles.errors').length > 0) {
+        this.set('validationsClass', 'has-error');
+      } else if (this.model.get('validations.attrs.titles.warnings').length > 0) {
+        this.set('validationsClass', 'has-warning');
+      } else {
+        this.set('validationsClass', 'has-success');
+      }
     }
   }
 });
