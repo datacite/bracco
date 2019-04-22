6599dde4a094697edb620b1823e72970debce42f
Martin Fenner <martin.fenner@datacite.org>
2019-04-22 23:34:12 +0200

    disable form if errors with nameIdentifiers

--- app/helpers/model-disabled.js
+++ app/helpers/model-disabled.js
@@ -1,7 +1,10 @@
 import { helper } from '@ember/component/helper';
 
-export function modelDisabled([isValid, state]) {
-  return !(isValid || state === "draft");
+export function modelDisabled([isValid, state, creators]) {
+  let nameIdentifiersErrors = creators.filter(function(creator) {
+    return creator.get('validations.attrs.nameIdentifiers.errors').length > 0;
+  });
+  return !(isValid || state === "draft") || nameIdentifiersErrors.length > 0;
 }
 
 export default helper(modelDisabled);
