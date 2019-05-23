ef486f154daa55cc2128fb1ed320eda7675382b7
Martin Fenner <martin.fenner@datacite.org>
2019-05-23 23:14:33 +0200

    simplified display of contact information. #195

--- app/models/contact.js
+++ app/models/contact.js
@@ -1,32 +1,32 @@
 import attr from 'ember-data/attr';
 import Fragment from 'ember-data-model-fragments/fragment';
-import { validator, buildValidations } from 'ember-cp-validations';
+// import { validator, buildValidations } from 'ember-cp-validations';
 
-const Validations = buildValidations({
-  'email': [
-    validator('presence', {
-      presence: true,
-      allowBlank: true
-    }),
-    validator('format', {
-      type: 'email',
-      allowNonTld: true,
-      message: 'Please enter a valid email address.'
-    })
-  ],
-  'givenName': validator('presence', {
-    presence: true,
-    allowBlank: true
-  }),
-  'familyName': validator('presence', {
-    presence: true,
-    allowBlank: true
-  })
-}, {
-  isWarning: true
-});
+// const Validations = buildValidations({
+//   'email': [
+//     validator('presence', {
+//       presence: true,
+//       allowBlank: true
+//     }),
+//     validator('format', {
+//       type: 'email',
+//       allowNonTld: true,
+//       message: 'Please enter a valid email address.'
+//     })
+//   ],
+//   'givenName': validator('presence', {
+//     presence: true,
+//     allowBlank: true
+//   }),
+//   'familyName': validator('presence', {
+//     presence: true,
+//     allowBlank: true
+//   })
+// }, {
+//   isWarning: true
+// });
 
-export default Fragment.extend(Validations, {
+export default Fragment.extend({
   email : attr('string'),
   givenName : attr('string'),
   familyName  : attr('string')
