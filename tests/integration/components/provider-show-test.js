5f3fe4d62c0b804860ded860b5d2b3ade71cf061
Richard Hallett <rhallett@datacite.org>
2019-05-23 14:16:49 +0200

    Update test to match changes for provider show

--- tests/integration/components/provider-show-test.js
+++ tests/integration/components/provider-show-test.js
@@ -18,6 +18,6 @@ module('Integration | Component | provider-show', function(hooks) {
       {{/provider-show}}
     `);
 
-    assert.dom('*').hasText('Provider ID Contact Name Dr. Steven McEachern Contact Email ada@anu.edu.au Billing Information Additional Contacts Password has not been set');
+    assert.dom('*').hasText('Organization Information Provider ID Contact Name Dr. Steven McEachern Service Contact Information General Contact Email ada@anu.edu.au Password has not been set Billing Information');
   });
 });
