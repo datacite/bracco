5934da426a9c6df8d47025779a0e55a96a147c9a
Richard Hallett <rhallett@datacite.org>
2019-05-23 18:14:25 +0200

    Add better handling for empty data in contacts for API.

--- app/serializers/contact.js
+++ app/serializers/contact.js
@@ -1,9 +1,10 @@
 import DS from 'ember-data';
+import { isEmpty } from '@ember/utils';
 
 export default DS.JSONSerializer.extend({
   serializeAttribute(snapshot, json, key) {
-    // Don't try and send null field contact details to the API
-    if(snapshot.attr(key) != null) {
+    // Don't try and send empty field contact details to the API
+    if(!isEmpty(snapshot.attr(key))) {
       this._super(...arguments);
     }
   }
