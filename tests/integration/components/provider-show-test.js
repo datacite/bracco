cd079ca008928b82be5a9bf2efe2863bc9701e9d
Martin Fenner <martin.fenner@datacite.org>
2019-06-23 01:32:50 +0200

    update tests

--- tests/integration/components/provider-show-test.js
+++ tests/integration/components/provider-show-test.js
@@ -18,6 +18,6 @@ module('Integration | Component | provider-show', function(hooks) {
       {{/provider-show}}
     `);
 
-    assert.dom('*').hasText('Organization Information Provider ID Organization Name to Display System Email Service Contact Information Password has not been set Billing Information has not been provided');
+    assert.dom('*').hasText('Organization Information Provider ID Organization Name Australian Data Archive System Email Service Contact Information Password has not been set Billing Information has not been provided');
   });
 });
