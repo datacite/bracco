e85b8dd370b0f5ecdb486583e2540b6b122a72fd
Martin Fenner <martin.fenner@datacite.org>
2019-07-30 10:42:53 +0200

    show facets repository-type and certificate for repositories. datacite/datacite#797

--- app/controllers/providers/show/repositories.js
+++ app/controllers/providers/show/repositories.js
@@ -1,9 +1,11 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-  queryParams: ['query', 'year', 'software', 'sort', 'page', 'size'],
+  queryParams: ['query', 'year', 'repository-type', 'certificate', 'software', 'sort', 'page', 'size'],
   query: null,
   year: null,
+  'repository-type': null,
+  certificate: null,
   software: null,
   sort: null,
   page: 1,
--- app/controllers/repositories/index.js
+++ app/controllers/repositories/index.js
@@ -1,11 +1,13 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-  queryParams: ['query', 'year', 'provider-id', 'client-type', 'software', 'include-deleted', 'sort', 'page', 'size'],
+  queryParams: ['query', 'year', 'provider-id', 'client-type', 'repository-type', 'certificate', 'software', 'include-deleted', 'sort', 'page', 'size'],
   query: null,
   year: null,
   'provider-id': null,
   'client-type': null,
+  'repository-type': null,
+  certificate: null,
   software: null,
   'include-deleted': null,
   sort: null,
