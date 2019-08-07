853a8e8fa9a6e2da480bff29b00b32ed4934367d
Martin Fenner <martin.fenner@datacite.org>
2019-08-07 14:28:59 +0200

    support faceting by non-profit-status. datacite/datacite#807

--- app/controllers/providers/index.js
+++ app/controllers/providers/index.js
@@ -1,13 +1,14 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-  queryParams: ['query', 'region', 'member-type', 'organization-type', 'focus-area', 'include-deleted', 'year', 'sort', 'page', 'size'],
+  queryParams: ['query', 'region', 'member-type', 'organization-type', 'focus-area', 'include-deleted', 'non-profit-status', 'year', 'sort', 'page', 'size'],
   query: null,
   region: null,
   'member-type': null,
   'organization-type': null,
   'focus-area': null,
   'include-deleted': null,
+  'non-profit-status': null,
   year: null,
   sort: null,
   page: 1,
--- app/controllers/providers/show/organizations.js
+++ app/controllers/providers/show/organizations.js
@@ -1,13 +1,14 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-  queryParams: ['query', 'region', 'consortium-id', 'organization-type', 'focus-area', 'include-deleted', 'year', 'sort', 'page', 'size'],
+  queryParams: ['query', 'region', 'consortium-id', 'organization-type', 'focus-area', 'include-deleted', 'non-profit-status', 'year', 'sort', 'page', 'size'],
   query: null,
   region: null,
   'consortium-id': null,
   'organization-type': null,
   'focus-area': null,
   'include-deleted': null,
+  'non-profit-status': null,
   year: null,
   sort: null,
   page: 1,
