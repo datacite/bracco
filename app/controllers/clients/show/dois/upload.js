9d8c0bd748f840b50dd6fe232b822ef9dfc825db
Kristian Garza <kj.garza@gmail.com>
2019-08-14 11:05:01 +0200

    send null values for titles and description when changing
    In the REST API  JSON attributes always overwrite what is provided via the xmlattribute. Therefore upload files updates should always send null values for all other attributes other than xml.
    
    https://github.com/datacite/datacite/issues/811

--- app/controllers/clients/show/dois/upload.js
+++ app/controllers/clients/show/dois/upload.js
@@ -15,6 +15,12 @@ export default Controller.extend({
     submit(doi) {
       doi.set('event', this.setEvent(doi.get('state')));
 
+      doi.set('titles', null);
+      doi.set('descriptions', null);
+      doi.set('types', null);
+      doi.set('creators', null);
+      doi.set('rightsList', null);
+
       let self = this;
       doi.save().then(function(doi) {
         self.transitionToRoute('clients.show.dois.show', doi.get('client').get('id'), doi);
