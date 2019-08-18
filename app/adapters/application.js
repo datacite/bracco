1dcb6cb433d3872485da52cc8236fdd8276f870b
Martin Fenner <martin.fenner@datacite.org>
2019-08-18 11:59:01 +0200

    authenticate without jquery

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,19 +1,23 @@
 import DS from 'ember-data';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 import { inject as service } from '@ember/service';
-import { isPresent } from '@ember/utils';
 import ENV from 'bracco/config/environment';
+import { computed } from '@ember/object';
+import { isPresent } from '@ember/utils';
 
 const { JSONAPIAdapter } = DS;
 
 export default JSONAPIAdapter.extend(DataAdapterMixin, {
   session: service(), 
   host: ENV.API_URL,
-  
-  authorize(xhr) {
+
+  headers: computed('session.data.authenticated.token', function() {
+    const headers = {};
     let { access_token } = this.get('session.data.authenticated');
     if (isPresent(access_token)) {
-      xhr.setRequestHeader('Authorization', `Bearer ${access_token}`);
+      headers['Authorization'] = `Bearer ${access_token}`;
     }
-  }
+
+    return headers;
+  }),
 });
