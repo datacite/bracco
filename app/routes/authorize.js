9bbdc068a425b8e9a8ca882f73f5d960e39add3c
Martin Fenner <martin.fenner@datacite.org>
2019-08-21 15:28:21 +0200

    use fastboot server for authorization

--- app/routes/authorize.js
+++ app/routes/authorize.js
@@ -6,16 +6,19 @@ export default Route.extend({
   fastboot: service(),
 
   model() {
-    let headers = this.get('fastboot.request.headers');
-    let jwt = headers.get('x-amzn-oidc-data');
-    if (jwt) {
-      let self = this;
-      this.session.authenticate('authenticator:globus', jwt).catch((reason) => {
-        self.set('errorMessage', reason.errors && reason.errors[0].title || reason);
-        self.transitionTo('/');
-      });
-    } else {
-      this.transitionTo('/sign-in?globus');
+    // force fastboot
+    if(this.get('fastboot.isFastBoot')) {
+      let headers = this.get('fastboot.request.headers');
+      let jwt = headers.get('x-amzn-oidc-data');
+      if (jwt) {
+        let self = this;
+        this.session.authenticate('authenticator:globus', jwt).catch((reason) => {
+          self.set('errorMessage', reason.errors && reason.errors[0].title || reason);
+          self.transitionTo('/');
+        });
+      } else {
+        this.transitionTo('/sign-in?globus');
+      }
     }
   }
 });
