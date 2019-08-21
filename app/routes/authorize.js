80e62f3175e6bb46f21d1dd067569bb934a200b6
Martin Fenner <martin.fenner@datacite.org>
2019-08-21 18:18:48 +0200

    use fetch to get oidc jwt

--- app/routes/authorize.js
+++ app/routes/authorize.js
@@ -1,26 +1,29 @@
 import Route from '@ember/routing/route';
+import fetch from 'fetch';
+import ENV from 'bracco/config/environment';
 import { inject as service } from '@ember/service';
 
 export default Route.extend({
   session: service(),
-  fastboot: service(),
 
   model() {
-    // force fastboot
-    if(this.get('fastboot.isFastBoot')) {
-      let headers = this.get('fastboot.request.headers');
-      let jwt = headers.get('x-amzn-oidc-data');
+    let self = this;
+    let url = ENV.FABRICA_URL + '/authorize';
+    fetch(url).then(function (response) {
+      let jwt = response.headers.get('x-amzn-oidc-data');
+
       if (jwt) {
-        let self = this;
-        this.session.authenticate('authenticator:globus', jwt).catch((reason) => {
+        self.session.authenticate('authenticator:globus', jwt).catch((reason) => {
           self.set('errorMessage', reason.errors && reason.errors[0].title || reason);
           self.transitionTo('/');
         });
       } else {
-        this.transitionTo('/sign-in?globus');
+        console.log(response);
+        self.transitionTo('/sign-in?globus');
       }
-    } else {
-      this.transitionTo('/sign-in');
-    }
+    }).catch(function (error) {
+      console.log(error);
+      this.transitionTo('/sign-in?globus');
+    });
   }
 });
