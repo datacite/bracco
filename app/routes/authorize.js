8fe9dc610fc3d5da0945c7ca7de07a1a0995a572
Martin Fenner <martin.fenner@datacite.org>
2019-08-21 10:32:10 +0200

    use fastboot headers. datacite/datacite#829

--- app/routes/authorize.js
+++ app/routes/authorize.js
@@ -1,29 +1,21 @@
 import Route from '@ember/routing/route';
-import fetch from 'fetch';
-import ENV from 'bracco/config/environment';
 import { inject as service } from '@ember/service';
 
 export default Route.extend({
   session: service(),
+  fastboot: service(),
 
   beforeModel() {
-    let self = this;
-    let url = ENV.FABRICA_URL + '/authorize';
-    fetch(url).then(function (response) {
-      let jwt = response.headers.get('x-amzn-oidc-data');
-
-      if (jwt) {
-        self.session.authenticate('authenticator:globus', jwt).catch((reason) => {
-          self.set('errorMessage', reason.errors && reason.errors[0].title || reason);
-          self.transitionTo('/');
-        });
-      } else {
-        console.log(response);
-        self.transitionTo('/sign-in?globus');
-      }
-    }).catch(function (error) {
-      console.log(error);
+    let headers = this.get('fastboot.request.headers');
+    let jwt = headers.get('x-amzn-oidc-data');
+    if (jwt) {
+      let self = this;
+      this.session.authenticate('authenticator:globus', jwt).catch((reason) => {
+        self.set('errorMessage', reason.errors && reason.errors[0].title || reason);
+        self.transitionTo('/');
+      });
+    } else {
       this.transitionTo('/sign-in?globus');
-    });
+    }
   }
 });
