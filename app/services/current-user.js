bd473bbe5a2f5cc57fec99e890fd725440a0e0c3
Martin Fenner <martin.fenner@datacite.org>
2019-08-22 22:45:54 +0200

    handle personal accounts. datacite/datacite#829

--- app/services/current-user.js
+++ app/services/current-user.js
@@ -23,6 +23,7 @@ export default Service.extend({
   isAdmin: false,
   isProvider: false,
   isClient: false,
+  isResearcher: false,
 
   load() {
     if (this.get('session.data.authenticated.access_token')) {
@@ -76,8 +77,13 @@ export default Service.extend({
         this.set('settings', { route: 'clients.show.settings', id: this.uid });
         this.set('roleName', 'Repository');
       } else if (payload.role_id === 'user') {
-        this.set('home', { route: 'password' });
         this.set('roleName', 'User');
+      }
+
+      if (payload.uid.startsWith('0')) {
+        this.set('isResearcher', true);
+        this.set('home', { route: 'researchers.show', id: this.uid });
+        this.set('settings', { route: 'researchers.show.settings', id: this.uid });
       }
 
       if (payload.role_id !== 'user') {
