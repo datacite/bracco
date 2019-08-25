672342739051c59ccd5ac0c97ca5a8b06283f03d
Martin Fenner <martin.fenner@datacite.org>
2019-08-25 11:36:34 +0200

    force production environment in fastboot

--- fastboot-server.js
+++ fastboot-server.js
@@ -1,13 +1,11 @@
 const FastBootAppServer = require('fastboot-app-server');
  
-const MY_GLOBAL = 'MY GLOBAL';
- 
 let server = new FastBootAppServer({
   distPath: 'dist',
   gzip: true,
   host: '0.0.0.0',
   port: 80,
-  sandboxGlobals: { GLOBAL_VALUE: MY_GLOBAL }, // Optional - Make values available to the Ember app running in the FastBoot server, e.g. "MY_GLOBAL" will be available as "GLOBAL_VALUE"
+  sandboxGlobals: { PASSENGER_APP_ENV: 'production' },
   chunkedResponse: true
 });
  
