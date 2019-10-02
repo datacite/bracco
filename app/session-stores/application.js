c2b10ac2234a417e08bd9f0d12c505346f446ea7
Martin Fenner <martin.fenner@datacite.org>
2019-10-02 07:48:53 +0200

    temporarily use separate cookie. datacite/datacite#836

--- app/session-stores/application.js
+++ app/session-stores/application.js
@@ -1,7 +1,5 @@
 import CookieStore from 'ember-simple-auth/session-stores/cookie';
-import ENV from 'bracco/config/environment';
 
 export default CookieStore.extend({
-  cookieName: '_datacite',
-  cookieDomain: ENV.COOKIE_DOMAIN
+  cookieName: '_fabrica'
 });
