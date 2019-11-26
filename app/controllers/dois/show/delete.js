3bf95ec7f14916d92a3e5fd87e38cea2d7260bbd
Martin Fenner <martin.fenner@datacite.org>
2019-11-26 09:53:25 +0100

    redirect to repository doi listing when deleting doi

--- app/controllers/dois/show/delete.js
+++ app/controllers/dois/show/delete.js
@@ -4,15 +4,15 @@ import { inject as service } from '@ember/service';
 export default Controller.extend({
   store: service(),
 
-  repository: null,
+  repositoryId: null,
 
   actions: {
     destroy() {
-      this.set('repository', this.model.get('repository'));
+      this.set('repositoryId', this.model.get('repository.id'));
       let self = this;
       this.store.findRecord("doi", this.model.get('id'), { backgroundReload: false }).then(function(doi) {
         doi.destroyRecord().then(function () {
-          self.transitionToRoute('dois');
+          self.transitionToRoute('repositories.show.dois', self.get('repositoryId'));
         });
       });
     },
--- app/controllers/repositories/show/dois/show/delete.js
+++ app/controllers/repositories/show/dois/show/delete.js
@@ -4,15 +4,15 @@ import { inject as service } from '@ember/service';
 export default Controller.extend({
   store: service(),
 
-  client: null,
+  repositoryId: null,
 
   actions: {
     destroy() {
-      this.set('repository', this.model.get('repository'));
+      this.set('repositoryId', this.model.get('repository.id'));
       let self = this;
       this.store.findRecord("doi", this.model.get('id'), { backgroundReload: false }).then(function(doi) {
         doi.destroyRecord().then(function () {
-          self.transitionToRoute('repositories.show.dois', self.get('repository.id'));
+          self.transitionToRoute('repositories.show.dois', self.get('repositoryId'));
         });
       });
     },
