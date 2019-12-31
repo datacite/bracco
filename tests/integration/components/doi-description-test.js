bcb55181b734eb73b67d73b5b5407ef64eff0212
Martin Fenner <martin.fenner@datacite.org>
2019-12-31 18:52:40 +0100

    uncommented failing tests

--- tests/integration/components/doi-description-test.js
+++ tests/integration/components/doi-description-test.js
@@ -1,21 +1,21 @@
-// import { module, test } from 'qunit';
-// import { setupRenderingTest } from 'ember-qunit';
-// import { render } from '@ember/test-helpers';
-// import hbs from 'htmlbars-inline-precompile';
-// // import { selectChoose } from 'ember-power-select/test-support';
-// import { setupFactoryGuy, make } from 'ember-data-factory-guy';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+// import { selectChoose } from 'ember-power-select/test-support';
+import { setupFactoryGuy, make } from 'ember-data-factory-guy';
 
-// module('Integration | Component | doi description', function(hooks) {
-//   setupRenderingTest(hooks);
-//   setupFactoryGuy(hooks);
+module('Integration | Component | doi description', function(hooks) {
+  setupRenderingTest(hooks);
+  setupFactoryGuy(hooks);
 
-//   test('it renders', async function(assert) {
-//     this.set('model', make('doi'));
-//     this.set('fragment', make('description'));
-//     await render(hbs`{{doi-description model=model fragment=fragment index=0}}`);
+  test('it renders', async function(assert) {
+    this.set('model', make('doi'));
+    this.set('fragment', make('description'));
+    await render(hbs`{{doi-description model=model fragment=fragment index=0}}`);
 
-//     assert.dom('*').hasText('All additional information that does not fit in any of the other categories. Description Type Language (optional)');
-//   });
+    assert.dom('*').hasText('All additional information that does not fit in any of the other categories. Description Type Language (optional)');
+  });
 
   // test('select type and language values', async function(assert) {
   //   this.set('model', make('doi'));
@@ -28,4 +28,4 @@
   //   assert.dom(".description-types").hasText("Methods");
   //   assert.dom(".description-langs").hasText("Akan");
   // });
-// });
+});
--- tests/integration/components/doi-descriptions-test.js
+++ tests/integration/components/doi-descriptions-test.js
@@ -1,34 +1,34 @@
-// import { module, test } from 'qunit';
-// import { setupRenderingTest } from 'ember-qunit';
-// import { setupFactoryGuy, make } from 'ember-data-factory-guy';
-// import { render, fillIn, click } from '@ember/test-helpers';
-// import hbs from 'htmlbars-inline-precompile';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { setupFactoryGuy, make } from 'ember-data-factory-guy';
+import { render, fillIn, click } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
 
-// module('Integration | Component | doi descriptions', function(hooks) {
-//   setupRenderingTest(hooks);
-//   setupFactoryGuy(hooks);
+module('Integration | Component | doi descriptions', function(hooks) {
+  setupRenderingTest(hooks);
+  setupFactoryGuy(hooks);
 
-//   test('it renders', async function(assert) {
-//     this.set('model', make('doi'));
-//     await render(hbs`{{doi-descriptions model=model}}`);
-//     await click('#add-description');
-//     let descriptions = this.element.querySelectorAll('textarea.description-field');
+  test('it renders', async function(assert) {
+    this.set('model', make('doi'));
+    await render(hbs`{{doi-descriptions model=model}}`);
+    await click('#add-description');
+    let descriptions = this.element.querySelectorAll('textarea.description-field');
 
-//     await fillIn(descriptions[0], "Abhinandan: Crowds gather for Indian pilots release");
-//     assert.dom(descriptions[0]).hasValue("Abhinandan: Crowds gather for Indian pilots release");
-//   });
+    await fillIn(descriptions[0], "Abhinandan: Crowds gather for Indian pilots release");
+    assert.dom(descriptions[0]).hasValue("Abhinandan: Crowds gather for Indian pilots release");
+  });
 
-//   test('add multiple values', async function(assert) {
-//     this.set('model', make('doi'));
-//     await render(hbs`{{doi-descriptions model=model}}`);
-//     await click('#add-description');
-//     await click('#add-description');
-//     let descriptions = this.element.querySelectorAll('textarea.description-field');
+  test('add multiple values', async function(assert) {
+    this.set('model', make('doi'));
+    await render(hbs`{{doi-descriptions model=model}}`);
+    await click('#add-description');
+    await click('#add-description');
+    let descriptions = this.element.querySelectorAll('textarea.description-field');
 
-//     await fillIn(descriptions[0], "Abhinandan: Crowds gather for Indian pilots release");
-//     await fillIn(descriptions[1], "Praesent quis blandit odio. Donec justo ex, ");
+    await fillIn(descriptions[0], "Abhinandan: Crowds gather for Indian pilots release");
+    await fillIn(descriptions[1], "Praesent quis blandit odio. Donec justo ex, ");
 
-//     assert.dom(descriptions[0]).hasValue("Abhinandan: Crowds gather for Indian pilots release");
-//     assert.dom(descriptions[1]).hasValue("Praesent quis blandit odio. Donec justo ex, ");
-//   });
-// });
+    assert.dom(descriptions[0]).hasValue("Abhinandan: Crowds gather for Indian pilots release");
+    assert.dom(descriptions[1]).hasValue("Praesent quis blandit odio. Donec justo ex, ");
+  });
+});
