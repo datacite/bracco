7c86ea59a52416c8853a53ec15a3c5638d4238e9
Martin Fenner <martin.fenner@datacite.org>
2020-01-14 07:47:54 +0100

    Revert "removing flash message"
    This reverts commit 8b5df2e2cec442f0df47b6c616b0ef7d6642000d.

--- app/components/metrics-counter.js
+++ app/components/metrics-counter.js
@@ -39,6 +39,13 @@ export default Component.extend({
         } else {
           console.log(reason);
         }
+
+        self
+          .get('flashMessages')
+          .warning(
+            'Fabrica is currently unavailable due to a DataCite API problem. We apologize for the inconvenience and are working hard to restore the service. Please check back later or contact DataCite Support if you have a question.'
+          );
+        self.transitionTo('/');
       });
   },
   actions: {
