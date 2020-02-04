306c688b53a5ef2b67b1d4ee43d644b2f3d3823c
kjgarza <kgarza@datacite.org>
2020-02-04 15:31:51 +0100

    move check to the template

--- app/components/doi-summary.js
+++ app/components/doi-summary.js
@@ -6,28 +6,6 @@ export default Component.extend({
   tagName: 'div',
   classNames: [ 'panel-body' ],
   isList: false,
-  citations: 0,
-  views: 0,
-  downloads: 0,
   store: service(),
-  hasUsage: computed('views','downloads', function() {
-    return ((this.views + this.downloads) > 0) ?  true  : false;
-  }),
-  hasMetrics: computed('views','downloads','citations',function() {
-    return ((this.views + this.downloads + this.citations) > 0) ?  true  : false;
-  }),
-  isPerson: computed(function() {
-    return this.attrs.link == 'users.show.dois';
-  }),
-  didReceiveAttrs() {
-    this._super(...arguments);
-
-    this.passMetrics();
-  },
-  passMetrics() {
-    let self = this;
-    self.set('citations' , this.model.citations || 0);
-    self.set('views', this.model.views || 0);
-    self.set('downloads',this.model.downloads || 0);
-  },
+  isResearcherProfile: false,
 });
