3a1facf7ee17482fb2021a465e89db8d32fe5b74
Martin Fenner <martin.fenner@datacite.org>
2020-02-24 21:28:24 +0100

    don't validate creators and titles in file upload form. #265

--- app/helpers/doi-form-errors.js
+++ app/helpers/doi-form-errors.js
@@ -11,16 +11,19 @@ export function doiFormErrors([ model ]) {
   let errorAttributes = model.validations.errors.mapBy('attribute');
 
   // check validation errors for embed data model fragments
-  if (model.titles) {
-    model.titles.forEach((title) => {
-      errorAttributes = errorAttributes.concat(title.validations.errors.mapBy('attribute'));
-    });
+  if ([ 'new', 'edit' ].includes(model.mode)) {
+    if (model.titles) {
+      model.titles.forEach((title) => {
+        errorAttributes = errorAttributes.concat(title.validations.errors.mapBy('attribute'));
+      });
+    }
+    if (model.creators) {
+      model.creators.forEach((creator) => {
+        errorAttributes = errorAttributes.concat(creator.validations.errors.mapBy('attribute'));
+      });
+    }
   }
-  if (model.creators) {
-    model.creators.forEach((creator) => {
-      errorAttributes = errorAttributes.concat(creator.validations.errors.mapBy('attribute'));
-    });
-  }
+
   return errorAttributes.map(function(attribute) {
     return labelList[attribute] || attribute;
   }).join(', ');
