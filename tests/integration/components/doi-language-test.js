0466a0385cec5b0f898c9d8d44ecec9f6483a003
kjgarza <kgarza@datacite.org>
2020-02-25 14:58:21 +0100

    added acceptance tests
    - see the form in edit mode
    - see new form

--- tests/integration/components/doi-language-test.js
+++ tests/integration/components/doi-language-test.js
@@ -2,7 +2,6 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
-// import { selectChoose } from 'ember-power-select/test-support/helpers';
 
 
 import { setupFactoryGuy, make } from 'ember-data-factory-guy';
@@ -15,12 +14,7 @@ module('Integration | Component | doi language', function(hooks) {
     this.set('model', make('doi'));
     console.log(this.set('model', make('doi')));
     await render(hbs`{{doi-language model=model.doi}}`);
-    // await selectChoose('.doi-language', '.ember-power-select-option', 'English');
-
 
     assert.dom('*').hasText('Language (optional)  The primary language of the resource.');
-
-
-    // assert.dom('.ember-power-select-selected-item').hasText('English');
   });
 });
