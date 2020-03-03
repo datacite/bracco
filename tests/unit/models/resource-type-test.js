052b780ca0965921468e55d54e7162279aa7c025
Sarala Wimalaratne <sarala.dissanayake@gmail.com>
2020-03-03 08:22:24 +0000

    Model exists tests for all models

---
+++ tests/unit/models/resource-type-test.js
@@ -0,0 +1,12 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+import { run } from '@ember/runloop';
+
+module('Unit | Model | resource-type', function(hooks) {
+  setupTest(hooks);
+
+  test('it exists', function(assert) {
+    let model = run(() => this.owner.lookup('service:store').createRecord('resource-type'));
+    assert.ok(!!model);
+  });
+});
---
+++ tests/unit/models/title-test.js
@@ -0,0 +1,12 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+import { run } from '@ember/runloop';
+
+module('Unit | Model | title', function(hooks) {
+  setupTest(hooks);
+
+  test('it exists', function(assert) {
+    let model = run(() => this.owner.lookup('service:store').createRecord('title'));
+    assert.ok(!!model);
+  });
+});
