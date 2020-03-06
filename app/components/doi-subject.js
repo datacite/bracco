6481a04a474e5687d04d702fd5d9e91e78abde9b
kjgarza <kgarza@datacite.org>
2020-03-06 10:39:36 +0100

    full oecd list

--- app/components/doi-subject.js
+++ app/components/doi-subject.js
@@ -3,41 +3,62 @@ import ISO6391 from 'iso-639-1';
 import URI from 'urijs';
 import { isBlank } from '@ember/utils';
 // import OECD from 'helpers/oecd-list';
+// import { subjectList as OECD } from 'bracco/helpers/oecd-list';
 
+// const subjectList = OECD.subjects;
 const subjectList = [
-  'Pure mathematics',
-  'Applied mathematics',
-  'Statistics and probability',
-  'Computer sciences, information science and bioinformatics',
-  'Atomic, molecular and chemical physics',
-  'Condensed matter physics',
-  'Particles and fields physics',
-  'Nuclear physics', 'Fluids and plasma physics',
-  'Optics',
-  'Acoustics',
-  'Astronomy',
-  'Organic chemistry',
-  'Inorganic and nuclear chemistry',
-  'Physical chemistry',
-  'Polymer science',
-  'Electrochemistry',
-  'Colloid chemistry',
-  'Analytical chemistry',
-  'Geosciences, multidisciplinary', 'Mineralogy','Palaeontology','Geochemistry and geophysics',
-  'Physical geography',
-  'Geology',
-  'Volcanology',
-  'Environmental sciences',
-  'Meteorology and atmospheric sciences',
-  'climatic research',
-  'Oceanography',
-  'Hydrology',
-  'Water resources',
+  'Natural sciences',
+  'Mathematics',
+  'Computer and information sciences',
+  'Physical sciences',
+  'Chemical sciences',
+  'Earth and related Environmental sciences',
+  'Biological sciences',
+  'Other natural sciences',
+  'Engineering and technology',
+  'Civil engineering',
+  'Electrical engineering, Electronic engineering, Information engineering',
+  'Mechanical engineering',
+  'Chemical engineering',
+  'Materials engineering',
+  'Medical engineering',
+  'Environmental engineering',
+  'Environmental biotechnology',
+  'Industrial biotechnology',
+  'Nano-technology',
+  'Other engineering and technologies',
+  'Medical and Health sciences',
+  'Basic medicine',
+  'Clinical medicine',
+  'Health sciences',
+  'Medical biotechnology',
+  'Other medical sciences',
+  'Agricultural sciences',
+  'Agriculture, Forestry, and Fisheries',
+  'Animal and Dairy science',
+  'Veterinary science',
+  'Agricultural biotechnology',
+  'Other agricultural sciences',
+  'Social sciences',
+  'Psychology',
+  'Economics and Business',
+  'Educational sciences',
+  'Sociology',
+  'Law',
+  'Political science',
+  'Social and economic geography',
+  'Media and communications',
+  'Other social sciences',
+  'Humanities',
+  'History and Archaeology',
+  'Languages and Literature',
+  'Philosophy, Ethics and Religion',
+  'Arts (arts, history of arts, performing arts, music)',
+  'Other humanities',
 ];
 
 const oecdScheme = 'OECD';
 const oecdSchemeUri = 'http://www.oecd.org/science/inno/38235147.pdf';
-
 
 export default Component.extend({
   subjectList,
@@ -55,6 +76,15 @@ export default Component.extend({
   setScheme(value) {
     this.fragment.set('subjectScheme', value);
     this.set('subjectScheme', value);
+  },
+  didReceiveAttrs() {
+    this._super(...arguments);
+
+    if (subjectList.includes(this.fragment.get('subject'))) {
+      this.set('oecdSelected', true);
+    } else {
+      this.set('oecdSelected', false);
+    }
   },
 
   actions: {
