0e0f5e3433a36b8790614f81bba71000e7e0d5fc
Martin Fenner <martin.fenner@datacite.org>
2020-03-12 06:41:26 +0100

    only load admin user with admin account

--- app/routes/index.js
+++ app/routes/index.js
@@ -7,14 +7,16 @@ export default Route.extend({
   currentUser: service(),
 
   model() {
-    let self = this;
-    return this.store.findRecord('provider', 'admin').then(function(model) {
-      return model;
-    }).catch(function(reason) {
-      console.debug(reason);
+    if (this.can.can('read index')) {
+      let self = this;
+      return this.store.findRecord('provider', 'admin').then(function(model) {
+        return model;
+      }).catch(function(reason) {
+        console.debug(reason);
 
-      self.get('flashMessages').warning('Fabrica is currently unavailable due to a DataCite API problem. We apologize for the inconvenience and are working hard to restore the service. Please check back later or contact DataCite Support if you have a question.');
-    });
+        self.get('flashMessages').warning('Fabrica is currently unavailable due to a DataCite API problem. We apologize for the inconvenience and are working hard to restore the service. Please check back later or contact DataCite Support if you have a question.');
+      });
+    }
   },
 
   afterModel() {
