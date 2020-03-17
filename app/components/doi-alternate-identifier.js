91d2dcc76bd194a1d9a1b7319897d8f4b670f80b
kjgarza <kgarza@datacite.org>
2020-03-17 19:48:21 +0100

    remove un needed

--- app/components/doi-alternate-identifier.js
+++ app/components/doi-alternate-identifier.js
@@ -7,17 +7,12 @@ export default Component.extend({
   actions: {
     updateIdentifier(value) {
       this.fragment.set('identifier', value);
-      // this.setValidationClass();
     },
     updateIdentifierType(value) {
       this.fragment.set('identifierType', value);
-      // this.setValidationClass();
     },
     deleteIdentifier() {
       this.model.get('identifiers').removeObject(this.fragment);
-    },
-    selectIdentifierType(identifierType) {
-      this.fragment.set('identifierType', identifierType);
     },
   },
 });
