a34fb893a629077eb075dc87ec0f12067766775a
kjgarza <kgarza@datacite.org>
2020-03-25 15:59:09 +0100

    related Identifier fixes

--- tests/integration/components/doi-related-identifier-test.js
+++ tests/integration/components/doi-related-identifier-test.js
@@ -11,9 +11,9 @@ module('Integration | Component | doi related-identifier', function(hooks) {
 
   test('it renders', async function(assert) {
     this.set('model', make('doi'));
-    this.set('fragment', make('geoLocation'));
+    this.set('fragment', make('relatedIdentifier'));
     await render(hbs`{{doi-related-identifier model=model fragment=fragment index=0}}`);
 
-    assert.dom('*').hasText('Identifiers of related resources. These must be globally unique identifiers, such as: ARK, arXiv, bibcode, DOI, EAN13, EISSN, Handle, IGSN, ISBN, ISSN, ISTC, LISSN, LSID, PMID, PURL, UPC, URL, URN, w3id. Related Identifier Type Relation Type');
+    assert.dom('*').hasText('Identifiers of related resources. These must be globally unique identifiers, such as: ARK, arXiv, bibcode, DOI, EAN13, EISSN, Handle, IGSN, ISBN, ISSN, ISTC, LISSN, LSID, PMID, PURL, UPC, URL, URN, w3id. Related Identifier Type Relation Type Related Metadata Scheme The name of the scheme. Related Metadata Scheme URI The URI of the relatedMetadataScheme. Related Metadata Scheme Type The type of the relatedMetadataScheme, linked with the schemeURI.');
   });
 });
