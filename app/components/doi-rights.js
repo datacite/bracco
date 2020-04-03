c13b1f35d4c8c55d8f9d80f79ef1641b1d6ea375
kjgarza <kgarza@datacite.org>
2020-04-03 12:41:04 +0200

    remove unused code
    other unsued fields can be added later @kjgarza

--- app/components/doi-rights.js
+++ app/components/doi-rights.js
@@ -1,45 +1,23 @@
 import Component from '@ember/component';
-import { inject as service } from '@ember/service';
 import { isBlank } from '@ember/utils';
 
 export default Component.extend({
-
   isSpdxId: false,
   selected: [],
-  store: service(),
-
-  rightsArray: [],
 
   init() {
     this._super();
     this.set('spdxLicenseListComplete', this.spdxList);
     this.set('spdxLicenseList', this.spdxList);
   },
-
-  didReceiveAttrs() {
-    this._super(...arguments);
-
-    // if (this.spdxLicenseListComplete.includes(this.fragment.get('rights'))) {
-    //   this.set('isSpdxId', true);
-    // } else {
-    //   this.set('isSpdxId', false);
-    // }
-  },
   updateRights(rights) {
     if (rights instanceof String) {
       this.fragment.set('rights', null);
       this.fragment.set('rightsUri', null);
-      // // these fields are currently not supported bythe JSON API
-      // this.fragment.set('rightsIdentifier', null);
-      // this.fragment.set('schemeUri', null);
-      // this.fragment.set('rightsIdentifierScheme', null);
       this.set('isSpdxId', false);
     } else {
       this.fragment.set('rights', rights.name);
       this.fragment.set('rightsUri', rights.detailsUrl);
-      // this.fragment.set('rightsIdentifier', rights.licenseId);
-      // this.fragment.set('schemeUri', 'https://spdx.org/licenses/');
-      // this.fragment.set('rightsIdentifierScheme', 'SPDX');
       this.set('isSpdxId', true);
     }
   },
@@ -55,18 +33,9 @@ export default Component.extend({
         }
       }
     },
-    // updateRightsIdentifier(value) {
-    //   this.fragment.set('rightsIdentifier', value);
-    // },
     selectRights(value) {
       this.updateRights(value);
     },
-    // updateSchemeUri(value) {
-    //   this.fragment.set('schemeUri', value);
-    // },
-    // updateRightsIdentifierScheme(value) {
-    //   this.fragment.set('rightsIdentifierScheme', value);
-    // },
     updateRightsUri(value) {
       this.fragment.set('rightsUri', value);
     },
