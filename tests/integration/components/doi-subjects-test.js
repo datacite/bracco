f4e92f321ff74c2ec89a15dcda5eafa58f0473f6
kjgarza <kgarza@datacite.org>
2020-04-06 15:16:08 +0200

    eslint

--- tests/integration/components/doi-subjects-test.js
+++ tests/integration/components/doi-subjects-test.js
@@ -12,7 +12,7 @@ module('Integration | Component | doi subjects', function(hooks) {
   test('add multiple values', async function(assert) {
     this.set('subject', make('subject'));
     this.set('model', make('doi'));
-    this.set('model.subjects', [this.subject]);
+    this.set('model.subjects', [ this.subject ]);
     await render(hbs`{{doi-subjects model=model}}`);
     await click('#add-subject');
     await click('#add-subject');
