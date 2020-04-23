5b9d9a43ff1d41d2509d0885e2bff5304d05b800
Martin Fenner <martin.fenner@datacite.org>
2020-04-23 13:00:15 +0200

    cleanup doi saving

--- app/controllers/dois/show/edit.js
+++ app/controllers/dois/show/edit.js
@@ -6,6 +6,7 @@ import { A } from '@ember/array';
 export default Controller.extend({
   spdx: service(),
   store: service(),
+  features: service(),
 
   setEvent(stateChange) {
     if (stateChange[0] === 'draft' && stateChange[1] === 'registered') {
@@ -39,12 +40,7 @@ export default Controller.extend({
       // Don't try and set the landingPage information for DOI Updates
       doi.set('landingPage', null);
 
-      // only store subject with a subject text
-      doi.set('subjects', A(doi.get('subjects')).filter(function(subject) {
-        return !isBlank(subject.subject);
-      }));
-
-      // only store name identifiers and affiliations with a value
+      // only store name identifiers and affiliations for creators with a value
       A(doi.get('creators')).forEach((creator) => {
         creator.set('nameIdentifiers', A(creator.get('nameIdentifiers')).filter(function(nameIdentifier) {
           return !isBlank(nameIdentifier.nameIdentifier);
@@ -58,86 +54,85 @@ export default Controller.extend({
         }
       });
 
-
-      // only store identifiers with a  text
-      doi.set('contributors', A(doi.get('contributors')).filter(function(contributor) {
-        let nameIdentifiers = A(contributor.get('nameIdentifiers')).filter(function(nameIdentifier) {
-          return !isBlank(nameIdentifier.nameIdentifier);
-        });
-        let affiliation = A(contributor.get('affiliation')).filter(function(affiliation) {
-          return !isBlank(affiliation.name);
-        });
-
-        let contributorType = !isBlank(contributor.contributorType) ? [ contributor.contributorType ] : [];
-
-        return !isBlank(nameIdentifiers) || !isBlank(affiliation) || !isBlank(contributorType) || !isBlank(contributor.name);
-      }));
-
-      // only store identifiers with a  text
-      doi.set('relatedIdentifiers', A(doi.get('relatedIdentifiers')).filter(function(identifier) {
-        return !isBlank(identifier.relatedIdentifier) || !isBlank(identifier.relatedIdentifierType) || !isBlank(identifier.relationType) || !isBlank(identifier.relatedMetadataScheme) || !isBlank(identifier.resourceTypeGeneral);
-      }));
-
-      // only store identifiers with a  text
-      doi.set('fundingReferences', A(doi.get('fundingReferences')).filter(function(fundingReference) {
-        return !isBlank(fundingReference.funderName) || !isBlank(fundingReference.funderIdentifier) || !isBlank(fundingReference.funderIdentifierType) || !isBlank(fundingReference.awardNumber) || !isBlank(fundingReference.awardTitle);
-      }));
-
-      // only store identifiers with a  text
-      doi.set('rightsList', A(doi.get('rightsList')).filter(function(rights) {
-        return !isBlank(rights.rights);
-      }));
-
-      // only store identifiers with a  text
-      doi.set('dates', A(doi.get('dates')).filter(function(date) {
-        return !isBlank(date.date) || !isBlank(date.dateType) || !isBlank(date.dateInformation);
-      }));
-
-      // only store identifiers with a  text
-      doi.set('formats', A(doi.get('formats')).filter(function(format) {
-        return !isBlank(format);
-      }));
-
-      // only store identifiers with a  text
-      doi.set('sizes', A(doi.get('sizes')).filter(function(size) {
-        return !isBlank(size);
-      }));
-
-      // // only store descriptions with a description text
-      // doi.set('language', A(doi.get('language')).filter(function(language) {
-      //   return !isBlank(language);
-      // }));
-
-      // only store descriptions with a description text
-      doi.set('descriptions', A(doi.get('descriptions')).filter(function(description) {
-        return !isBlank(description.description);
-      }));
-
-      // only store identifiers with a  text
-      doi.set('identifiers', A(doi.get('identifiers')).filter(function(identifier) {
-        return !isBlank(identifier.identifier);
-      }));
-
       // only store titles with a title text
       doi.set('titles', A(doi.get('titles')).filter(function(title) {
         return !isBlank(title.title);
       }));
 
-      doi.set('geoLocations', A(doi.get('geoLocations')).filter(function(geoLocation) {
-        let point = isBlank(geoLocation.geoLocationPoint.pointLongitude) &&
-         isBlank(geoLocation.geoLocationPoint.pointLatitude) ? [] : [ geoLocation.geoLocationPoint ];
-        let box = isBlank(geoLocation.geoLocationBox.westBoundLongitude) &&
-        isBlank(geoLocation.geoLocationBox.eastBoundLongitude) &&
-        isBlank(geoLocation.geoLocationBox.southBoundLatitude) &&
-        isBlank(geoLocation.geoLocationBox.northBoundLatitude) ? [] : [ geoLocation.geoLocationBox ];
+      if (this.get('features').isEnabled('optionalFields')) {
+        // only store subject with a subject text
+        doi.set('subjects', A(doi.get('subjects')).filter(function(subject) {
+          return !isBlank(subject.subject);
+        }));
 
-        return !isBlank(geoLocation.geoLocationPlace) || !isBlank(point) || !isBlank(box);
-      }));
+        // only store name identifiers and affiliations for contributor with a value
+        doi.set('contributors', A(doi.get('contributors')).filter(function(contributor) {
+          let nameIdentifiers = A(contributor.get('nameIdentifiers')).filter(function(nameIdentifier) {
+            return !isBlank(nameIdentifier.nameIdentifier);
+          });
+          let affiliation = A(contributor.get('affiliation')).filter(function(affiliation) {
+            return !isBlank(affiliation.name);
+          });
 
+          let contributorType = !isBlank(contributor.contributorType) ? [ contributor.contributorType ] : [];
+
+          return !isBlank(nameIdentifiers) || !isBlank(affiliation) || !isBlank(contributorType) || !isBlank(contributor.name);
+        }));
+
+        // only store dates with a text
+        doi.set('dates', A(doi.get('dates')).filter(function(date) {
+          return !isBlank(date.date) || !isBlank(date.dateType) || !isBlank(date.dateInformation);
+        }));
+
+        // only store related identifiers with a text
+        doi.set('relatedIdentifiers', A(doi.get('relatedIdentifiers')).filter(function(identifier) {
+          return !isBlank(identifier.relatedIdentifier) || !isBlank(identifier.relatedIdentifierType) || !isBlank(identifier.relationType) || !isBlank(identifier.relatedMetadataScheme) || !isBlank(identifier.resourceTypeGeneral);
+        }));
+
+        // only store descriptions with a description text
+        doi.set('descriptions', A(doi.get('descriptions')).filter(function(description) {
+          return !isBlank(description.description);
+        }));
+
+        doi.set('geoLocations', A(doi.get('geoLocations')).filter(function(geoLocation) {
+          let point = isBlank(geoLocation.geoLocationPoint.pointLongitude) &&
+            isBlank(geoLocation.geoLocationPoint.pointLatitude) ? [] : [ geoLocation.geoLocationPoint ];
+          let box = isBlank(geoLocation.geoLocationBox.westBoundLongitude) &&
+            isBlank(geoLocation.geoLocationBox.eastBoundLongitude) &&
+            isBlank(geoLocation.geoLocationBox.southBoundLatitude) &&
+            isBlank(geoLocation.geoLocationBox.northBoundLatitude) ? [] : [ geoLocation.geoLocationBox ];
+
+          return !isBlank(geoLocation.geoLocationPlace) || !isBlank(point) || !isBlank(box);
+        }));
+
+        // only store identifiers with a  text
+        doi.set('identifiers', A(doi.get('identifiers')).filter(function(identifier) {
+          return !isBlank(identifier.identifier);
+        }));
+
+        // only store rights with a text
+        doi.set('rightsList', A(doi.get('rightsList')).filter(function(rights) {
+          return !isBlank(rights.rights);
+        }));
+
+        // only store sizes with a text
+        doi.set('sizes', A(doi.get('sizes')).filter(function(size) {
+          return !isBlank(size);
+        }));
+
+        // only store formats with a text
+        doi.set('formats', A(doi.get('formats')).filter(function(format) {
+          return !isBlank(format);
+        }));
+
+        // only store funding references with a text
+        doi.set('fundingReferences', A(doi.get('fundingReferences')).filter(function(fundingReference) {
+          return !isBlank(fundingReference.funderName) || !isBlank(fundingReference.funderIdentifier) || !isBlank(fundingReference.funderIdentifierType) || !isBlank(fundingReference.awardNumber) || !isBlank(fundingReference.awardTitle);
+        }));
+      }
 
       let self = this;
       doi.save().then(function(doi) {
-
         self.transitionToRoute('dois.show', doi);
       });
     },
--- app/controllers/repositories/show/dois/new.js
+++ app/controllers/repositories/show/dois/new.js
@@ -5,6 +5,7 @@ import { inject as service } from '@ember/service';
 
 export default Controller.extend({
   spdx: service(),
+  features: service(),
 
   setEvent(state) {
     if (state === 'registered') {
@@ -26,16 +27,6 @@ export default Controller.extend({
       // don't send xml
       doi.set('xml', null);
 
-      // only store subject with a subject text
-      doi.set('subjects', A(doi.get('subjects')).filter(function(subject) {
-        return !isBlank(subject.subject);
-      }));
-
-      // // only store subject with a subject text
-      // doi.set('formats', A(doi.get('formats')).filter(function(format) {
-      //   return !isBlank(format);
-      // }));
-
       // only store name identifiers and affiliations with a value
       A(doi.get('creators')).forEach((creator) => {
         creator.set('nameIdentifiers', A(creator.get('nameIdentifiers')).filter(function(nameIdentifier) {
@@ -50,95 +41,82 @@ export default Controller.extend({
         }
       });
 
-      // // only store name identifiers and affiliations with a value
-      // A(doi.get('contributors')).forEach((contributor) => {
-      //   contributor.set('nameIdentifiers', A(contributor.get('nameIdentifiers')).filter(function(nameIdentifier) {
-      //     return !isBlank(nameIdentifier.nameIdentifier);
-      //   }));
-      //   contributor.set('affiliation', A(contributor.get('affiliation')).filter(function(affiliation) {
-      //     return !isBlank(affiliation.name);
-      //   }));
-      //   if (contributor.nameType === 'Organizational') {
-      //     contributor.set('givenName', null);
-      //     contributor.set('familyName', null);
-      //   }
-      // });
-
-      // only store identifiers with a  text
-      doi.set('contributors', A(doi.get('contributors')).filter(function(contributor) {
-        let nameIdentifiers = A(contributor.get('nameIdentifiers')).filter(function(nameIdentifier) {
-          return !isBlank(nameIdentifier.nameIdentifier);
-        });
-        let affiliation = A(contributor.get('affiliation')).filter(function(affiliation) {
-          return !isBlank(affiliation.name);
-        });
-
-        let contributorType = !isBlank(contributor.contributorType) ? [ contributor.contributorType ] : [];
-
-        return !isBlank(nameIdentifiers) || !isBlank(affiliation) || !isBlank(contributorType) || !isBlank(contributor.name);
-      }));
-
-      // only store descriptions with a description text
-      doi.set('descriptions', A(doi.get('descriptions')).filter(function(description) {
-        return !isBlank(description.description);
-      }));
-
-
-      // only store identifiers with a  text
-      doi.set('identifiers', A(doi.get('identifiers')).filter(function(identifier) {
-        return !isBlank(identifier.identifier);
-      }));
-
-      // only store identifiers with a  text
-      doi.set('relatedIdentifiers', A(doi.get('relatedIdentifiers')).filter(function(identifier) {
-        return !isBlank(identifier.relatedIdentifier) || !isBlank(identifier.relatedIdentifierType) || !isBlank(identifier.relationType) || !isBlank(identifier.relatedMetadataScheme) || !isBlank(identifier.resourceTypeGeneral);
-      }));
-
-      // only store identifiers with a  text
-      doi.set('fundingReferences', A(doi.get('fundingReferences')).filter(function(fundingReference) {
-        return !isBlank(fundingReference.funderName) || !isBlank(fundingReference.funderIdentifier) || !isBlank(fundingReference.funderIdentifierType) || !isBlank(fundingReference.awardNumber) || !isBlank(fundingReference.awardTitle);
-      }));
-
-      // only store identifiers with a  text
-      doi.set('rightsList', A(doi.get('rightsList')).filter(function(rights) {
-        return !isBlank(rights.rights);
-      }));
-
-      // only store identifiers with a  text
-      doi.set('dates', A(doi.get('dates')).filter(function(date) {
-        return !isBlank(date.date) || !isBlank(date.dateType) || !isBlank(date.dateInformation);
-      }));
-
-      // only store identifiers with a  text
-      doi.set('formats', A(doi.get('formats')).filter(function(format) {
-        return !isBlank(format);
-      }));
-
-      // only store identifiers with a  text
-      doi.set('sizes', A(doi.get('sizes')).filter(function(size) {
-        return !isBlank(size);
-      }));
-
-      // only store descriptions with a description text
-      // doi.set('language', A(doi.get('language')).filter(function(language) {
-      //   return !isBlank(language);
-      // }));
-
       // only store titles with a title text
       doi.set('titles', A(doi.get('titles')).filter(function(title) {
         return !isBlank(title.title);
       }));
 
-      doi.set('geoLocations', A(doi.get('geoLocations')).filter(function(geoLocation) {
-        let point = isBlank(geoLocation.geoLocationPoint.pointLongitude) &&
-         isBlank(geoLocation.geoLocationPoint.pointLatitude) ? [] : [ geoLocation.geoLocationPoint ];
-        let box = isBlank(geoLocation.geoLocationBox.westBoundLongitude) &&
-        isBlank(geoLocation.geoLocationBox.eastBoundLongitude) &&
-        isBlank(geoLocation.geoLocationBox.southBoundLatitude) &&
-        isBlank(geoLocation.geoLocationBox.northBoundLatitude) ? [] : [ geoLocation.geoLocationBox ];
+      if (this.get('features').isEnabled('optionalFields')) {
+        // only store subject with a subject text
+        doi.set('subjects', A(doi.get('subjects')).filter(function(subject) {
+          return !isBlank(subject.subject);
+        }));
 
-        return !isBlank(geoLocation.geoLocationPlace) || !isBlank(point) || !isBlank(box);
-      }));
+        // only store name identifiers and affiliations for contributor with a value
+        doi.set('contributors', A(doi.get('contributors')).filter(function(contributor) {
+          let nameIdentifiers = A(contributor.get('nameIdentifiers')).filter(function(nameIdentifier) {
+            return !isBlank(nameIdentifier.nameIdentifier);
+          });
+          let affiliation = A(contributor.get('affiliation')).filter(function(affiliation) {
+            return !isBlank(affiliation.name);
+          });
+
+          let contributorType = !isBlank(contributor.contributorType) ? [ contributor.contributorType ] : [];
+
+          return !isBlank(nameIdentifiers) || !isBlank(affiliation) || !isBlank(contributorType) || !isBlank(contributor.name);
+        }));
+
+        // only store dates with a text
+        doi.set('dates', A(doi.get('dates')).filter(function(date) {
+          return !isBlank(date.date) || !isBlank(date.dateType) || !isBlank(date.dateInformation);
+        }));
+
+        // only store related identifiers with a text
+        doi.set('relatedIdentifiers', A(doi.get('relatedIdentifiers')).filter(function(identifier) {
+          return !isBlank(identifier.relatedIdentifier) || !isBlank(identifier.relatedIdentifierType) || !isBlank(identifier.relationType) || !isBlank(identifier.relatedMetadataScheme) || !isBlank(identifier.resourceTypeGeneral);
+        }));
+
+        // only store descriptions with a description text
+        doi.set('descriptions', A(doi.get('descriptions')).filter(function(description) {
+          return !isBlank(description.description);
+        }));
+
+        doi.set('geoLocations', A(doi.get('geoLocations')).filter(function(geoLocation) {
+          let point = isBlank(geoLocation.geoLocationPoint.pointLongitude) &&
+            isBlank(geoLocation.geoLocationPoint.pointLatitude) ? [] : [ geoLocation.geoLocationPoint ];
+          let box = isBlank(geoLocation.geoLocationBox.westBoundLongitude) &&
+            isBlank(geoLocation.geoLocationBox.eastBoundLongitude) &&
+            isBlank(geoLocation.geoLocationBox.southBoundLatitude) &&
+            isBlank(geoLocation.geoLocationBox.northBoundLatitude) ? [] : [ geoLocation.geoLocationBox ];
+
+          return !isBlank(geoLocation.geoLocationPlace) || !isBlank(point) || !isBlank(box);
+        }));
+
+        // only store identifiers with a  text
+        doi.set('identifiers', A(doi.get('identifiers')).filter(function(identifier) {
+          return !isBlank(identifier.identifier);
+        }));
+
+        // only store rights with a text
+        doi.set('rightsList', A(doi.get('rightsList')).filter(function(rights) {
+          return !isBlank(rights.rights);
+        }));
+
+        // only store sizes with a text
+        doi.set('sizes', A(doi.get('sizes')).filter(function(size) {
+          return !isBlank(size);
+        }));
+
+        // only store formats with a text
+        doi.set('formats', A(doi.get('formats')).filter(function(format) {
+          return !isBlank(format);
+        }));
+
+        // only store funding references with a text
+        doi.set('fundingReferences', A(doi.get('fundingReferences')).filter(function(fundingReference) {
+          return !isBlank(fundingReference.funderName) || !isBlank(fundingReference.funderIdentifier) || !isBlank(fundingReference.funderIdentifierType) || !isBlank(fundingReference.awardNumber) || !isBlank(fundingReference.awardTitle);
+        }));
+      }
 
       let self = this;
       doi.save().then(function(doi) {
