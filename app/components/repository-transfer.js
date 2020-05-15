bbab8d32731ec9b0b12f8b63e6e1c7651298dc30
kjgarza <kgarza@datacite.org>
2020-05-15 11:43:26 +0200

    fix: lint

--- app/components/repository-transfer.js
+++ app/components/repository-transfer.js
@@ -1,6 +1,5 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
-import { filter } from '@ember/object/computed';
 
 export default Component.extend({
   currentUser: service(),
