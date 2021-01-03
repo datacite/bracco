2994815f8ca8a90dc587c717a37ed0591537cba3
Martin Fenner <martin.fenner@datacite.org>
2021-01-03 06:50:53 +0100

    adjust charts to 2021. datacite/lupo#698

--- tests/integration/components/repository-info-test.js
+++ tests/integration/components/repository-info-test.js
@@ -3,12 +3,12 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | repository-info', function(hooks) {
+module('Integration | Component | repository-info', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
+  test('it renders', async function (assert) {
     await render(hbs`{{repository-info}}`);
 
-    assert.dom(this.element).hasText('DOIs by year 0 in 2020 20102020');
+    assert.dom(this.element).hasText('DOIs by year 0 in 2021 20112021');
   });
 });
