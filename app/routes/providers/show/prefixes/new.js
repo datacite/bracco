30ffa7ed124624d80d903118e211bdc1a9d1e3c2
Martin Fenner <martin.fenner@datacite.org>
2021-01-27 11:53:04 +0100

    only allow staff to assign new prefixes. #495

--- app/routes/providers/show/prefixes/new.js
+++ app/routes/providers/show/prefixes/new.js
@@ -10,14 +10,27 @@ export default Route.extend({
 
     return hash({
       provider: this.store.findRecord('provider', provider.get('id')),
-      'provider-prefix': this.store.createRecord('provider-prefix', { provider }),
+      'provider-prefix': this.store.createRecord('provider-prefix', {
+        provider
+      })
     });
   },
 
   setupController(controller, model) {
     this._super(controller, model);
 
-    this.controllerFor('providers.show.prefixes.new').send('searchPrefix', null);
+    this.controllerFor('providers.show.prefixes.new').send(
+      'searchPrefix',
+      null
+    );
   },
-});
 
+  afterModel() {
+    if (this.can.cannot('read index')) {
+      this.transitionTo(
+        'providers.show.prefixes',
+        this.modelFor('providers/show')
+      );
+    }
+  }
+});
