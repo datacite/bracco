ffb3f27f19320f5ffa208c73f3b23193d84b8e75
Martin Fenner <martin.fenner@datacite.org>
2021-02-02 11:09:24 +0100

    fix test

--- tests/integration/components/application-header-test.js
+++ tests/integration/components/application-header-test.js
@@ -3,12 +3,12 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | application header', function(hooks) {
+module('Integration | Component | application header', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
+  test('it renders', async function (assert) {
     await render(hbs`{{application-header}}`);
 
-    assert.dom('*').hasText('Toggle navigation Support Sign in');
+    assert.dom('*').hasText('Toggle navigation About Support Sign in');
   });
 });
