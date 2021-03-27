10b28d26863bb512ba946c866aaf7405800dcb3d
Martin Fenner <martin.fenner@datacite.org>
2021-03-27 09:44:58 +0100

    Update provider-form-errors.js

--- app/helpers/provider-form-errors.js
+++ app/helpers/provider-form-errors.js
@@ -28,36 +28,39 @@ const organizationLabelList = {
 export function providerFormErrors([model]) {
   let errorAttributes = model.validations.errors.mapBy('attribute');
 
-  // check validation errors for embed data model fragments
-  if (
-    model.serviceContact &&
-    isPresent(model.serviceContact.validations.errors)
-  ) {
-    errorAttributes = errorAttributes.concat(
-      'service contact ' +
-        model.serviceContact.validations.errors.mapBy('attribute')
-    );
-  }
-
-  // the following fields are disabled for consortium organizations
-  if (model.memberType !== 'consortium_organization') {
+  // check validation errors for embedded data model fragments
+  // but only if contacts exist
+  if (model.get('contacts').length > 0) {
     if (
-      model.billingContact &&
-      isPresent(model.billingContact.validations.errors)
+      model.serviceContact &&
+      isPresent(model.serviceContact.validations.errors)
     ) {
       errorAttributes = errorAttributes.concat(
-        'billing contact ' +
-          model.billingContact.validations.errors.mapBy('attribute')
+        'service contact ' +
+          model.serviceContact.validations.errors.mapBy('attribute')
       );
     }
-    if (
-      model.votingContact &&
-      isPresent(model.votingContact.validations.errors)
-    ) {
-      errorAttributes = errorAttributes.concat(
-        'voting contact ' +
-          model.votingContact.validations.errors.mapBy('attribute')
-      );
+
+    // the following fields are disabled for consortium organizations
+    if (model.memberType !== 'consortium_organization') {
+      if (
+        model.billingContact &&
+        isPresent(model.billingContact.validations.errors)
+      ) {
+        errorAttributes = errorAttributes.concat(
+          'billing contact ' +
+            model.billingContact.validations.errors.mapBy('attribute')
+        );
+      }
+      if (
+        model.votingContact &&
+        isPresent(model.votingContact.validations.errors)
+      ) {
+        errorAttributes = errorAttributes.concat(
+          'voting contact ' +
+            model.votingContact.validations.errors.mapBy('attribute')
+        );
+      }
     }
   }
 
