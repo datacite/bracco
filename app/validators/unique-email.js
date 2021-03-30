ee48afc1020a6d3dd6d9e9312b4b7d0e8b0533b9
Martin Fenner <martin.fenner@datacite.org>
2021-03-30 10:37:34 +0200

    also check deleted contacts for uniqueness. #529

--- app/validators/unique-email.js
+++ app/validators/unique-email.js
@@ -6,26 +6,42 @@ const UniqueEmail = BaseValidator.extend({
 
   validate(value, options, model) {
     let providerId = model.get('provider.id');
-    let query = `email:${value} !uid:${model.get('id')}`
+    let query = `email:${value} !uid:${model.get('id')}`;
 
-    // email should be unique per provider
-    return this.store.query('contact', { query, provider_id: providerId }).then((result) => {
-      if (result.content.length > 0) {
-        return 'The email address ' + value + ' already exists with a contact for provider ' + providerId.toUpperCase() + ', or existed before and has been deleted. Contact DataCite staff if you want to create an account with this email and provider ID.';
-      } else {
-        return true;
-      }
-    }).catch(function(reason) {
-      console.debug(reason);
-      return 'An error happened while looking up the email address ' + value + '.';
-    });
-  },
+    // email should be unique per provider,
+    // and that includes deleted records
+    return this.store
+      .query('contact', {
+        query,
+        provider_id: providerId,
+        'include-deleted': true
+      })
+      .then((result) => {
+        if (result.content.length > 0) {
+          return (
+            'The email address ' +
+            value +
+            ' already exists with a contact for provider ' +
+            providerId.toUpperCase() +
+            ', or existed before and has been deleted. Contact DataCite staff if you want to create an account with this email and provider ID.'
+          );
+        } else {
+          return true;
+        }
+      })
+      .catch(function (reason) {
+        console.debug(reason);
+        return (
+          'An error happened while looking up the email address ' + value + '.'
+        );
+      });
+  }
 });
 
 UniqueEmail.reopenClass({
   getDependentsFor() {
-    return [ 'id' ];
-  },
+    return ['id'];
+  }
 });
 
 export default UniqueEmail;
