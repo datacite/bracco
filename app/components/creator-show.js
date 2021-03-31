fb752b316f545fb9e8761377c3b6f607324aed9f
Suzanne Vogt <suzanne.vogt@datacite.org>
2021-03-31 15:13:33 -0400

    Issue #525 - The DOIs tab is not rendering correctly for inist.cea. (Limit creators in template.)

--- app/components/creator-show.js
+++ app/components/creator-show.js
@@ -3,13 +3,5 @@ import Component from '@ember/component';
 export default Component.extend({
   didReceiveAttrs() {
     this._super(...arguments);
-
-    let limit = 50;
-    let n = this.creators.length;
-    if (n > limit) {
-      for ( var i = (n-1); i >= limit; i--) {
-        this.creators.popObject;
-      }
-    }
   }
 });
