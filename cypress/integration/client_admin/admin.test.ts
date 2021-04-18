6bdf3b5f29819036973f5825fc1a6cb80a5c3297
Suzanne Vogt <suzanne.vogt@datacite.org>
2021-04-17 21:47:14 -0400

    Issue #516 - Convert acceptance tests from Polly to Cypress. (consortium_admin | admin and cleanup.)

--- cypress/integration/client_admin/admin.test.ts
+++ cypress/integration/client_admin/admin.test.ts
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 /* eslint-disable no-undef */
 
-describe('Acceptance: client_admin', () => {
+describe('Acceptance: client_admin | admin', () => {
   const waitTimeBetIt = 1000;
   const waitTime = 1000;
   const waitTime1 = 1000;
@@ -15,65 +15,68 @@ describe('Acceptance: client_admin', () => {
   it('is logged in', () => {
     cy.visit('/');
     cy.url().should('include', '/repositories/datacite.test')
+    cy.wait(waitTime);
+
     cy.get('ul.nav-tabs li.active a').should('contain', 'Settings')
+    cy.get('h2.work').contains('DataCite Test Repository');
     cy.get('a#account_menu_link').should('contain', 'DATACITE.TEST');
   });
 
   it('visiting homepage', () => {
     cy.visit('/');
     cy.url().should('include', '/repositories/datacite.test');
-    cy.get('h2.work').contains('DataCite Test Repository');
+    cy.wait(waitTime);
+
     cy.get('ul.nav-tabs li.active a').should('contain', 'Settings');
+    cy.get('h2.work').contains('DataCite Test Repository');
     cy.get('a#account_menu_link').should('contain', 'DATACITE.TEST');
   });
 
-  it('visiting info', () => {
-    cy.visit('/info');
-    cy.url().should('include', '/repositories/datacite.test');
+  it('visiting repository DataCite Test info', () => {
+    cy.visit('/repositories/datacite.test/info');
+    cy.url().should('include', '/repositories/datacite.test/info');
+    cy.wait(waitTime);
+
+    cy.contains('ul.nav-tabs li.active a', "Info");
     cy.get('h2.work').contains('DataCite Test Repository');
-    cy.get('ul.nav-tabs li.active a').should('contain', 'Settings');
-    cy.get('a#account_menu_link').should('contain', 'DATACITE.TEST');
+    cy.get('a#account_menu_link').contains('DATACITE.TEST');
   });
 
-  it('visiting providers', () => {
-    cy.visit('/providers');
+  it('visiting repository DataCite Test settings', () => {
+    cy.visit('/repositories/datacite.test');
     cy.url().should('include', '/repositories/datacite.test');
+    cy.wait(waitTime);
+
+    cy.contains('ul.nav-tabs li.active a', "Settings");
     cy.get('h2.work').contains('DataCite Test Repository');
-    cy.get('ul.nav-tabs li.active a').should('contain', 'Settings');
-    cy.get('a#account_menu_link').should('contain', 'DATACITE.TEST');
+    cy.get('a#account_menu_link').contains('DATACITE.TEST');
   });
 
+  it('visiting repository DataCite Test prefixes', () => {
+    cy.visit('/repositories/datacite.test/prefixes');
+    cy.url().should('include', '/repositories/datacite.test/prefixes');
+    cy.wait(waitTime);
 
-  it('visiting repositories', () => {
-    cy.visit('/repositories');
-    cy.url().should('include', '/repositories/datacite.test');
+    cy.contains('ul.nav-tabs li.active a', "Prefixes");
     cy.get('h2.work').contains('DataCite Test Repository');
-    cy.get('ul.nav-tabs li.active a').should('contain', 'Settings');
-    cy.get('a#account_menu_link').should('contain', 'DATACITE.TEST');
-  });
-
-
-  it('visiting settings', () => {
-    cy.visit('/');
-    cy.url().should('include', '/repositories/datacite.test');
-    cy.get('h2.work').contains('DataCite Test Repository');
-    cy.get('ul.nav-tabs li.active a').should('contain', 'Settings');
-    cy.get('a#account_menu_link').should('contain', 'DATACITE.TEST');
-  });
-
-  it('visiting prefixes', () => {
-    cy.visit('/prefixes');
-    cy.url().should('include', '/repositories/datacite.test');
-    cy.get('h2.work').contains('DataCite Test Repository');
-    cy.get('ul.nav-tabs li.active a').should('contain', 'Settings');
-    cy.get('a#account_menu_link').should('contain', 'DATACITE.TEST');
+    cy.get('a#account_menu_link').contains('DATACITE.TEST');
   });
 
   it('visiting dois', () => {
-    cy.visit('/dois');
-    cy.url().should('include', '/repositories/datacite.test');
+    cy.visit('/repositories/datacite.test/dois');
+    cy.url().should('include', '/repositories/datacite.test/dois');
+    cy.wait(waitTime);
+
+    cy.contains('ul.nav-tabs li.active a', "DOIs");
     cy.get('h2.work').contains('DataCite Test Repository');
-    cy.get('ul.nav-tabs li.active a').should('contain', 'Settings');
-    cy.get('a#account_menu_link').should('contain', 'DATACITE.TEST');
+    cy.get('a#account_menu_link').contains('DATACITE.TEST');
+  });
+
+  it('visiting specific doi', () => {
+    cy.visit('/dois/10.80225%2Fda52-7919');
+    cy.url().should('include', '/dois/10.80225%2Fda52-7919');
+    cy.wait(waitTime);
+
+    cy.contains('h2.work', "10.80225/da52-7919");
   });
 }];
--- cypress/integration/consortium_admin/admin.test.ts
+++ cypress/integration/consortium_admin/admin.test.ts
@@ -1,137 +1,166 @@
 /// <reference types="cypress" />
 /* eslint-disable no-undef */
 
-describe('Admin: Admin', () => {
-    beforeEach(() => {
-      cy.setCookie('_consent', 'true');
-      cy.setCookie('_fabrica', Cypress.env('consortium_admin_cookie'), { log: false });
-    });
-  
-    it('is logged in', () => {
-      cy.visit('/');
-      cy.location().should((loc) => {
-        expect(loc.pathname).to.eq('/');
-      });
-      cy.get('a#account_menu_link').contains('ADMIN');
-    });
-  /*
-    it('editing admin form', () => {
-      cy.visit('/edit');
-      cy.location().should((loc) => {
-        expect(loc.pathname).to.eq('/edit');
-      });
-      cy.get('input#member-id-field').should('exist');
-      cy.get('input#system-email-field').should('exist');
-      cy.get('input#website-field').should('exist');
-      cy.get('input#twitter-handle-field').should('exist');
-      cy.get('div#ror-id').should('exist');
-      cy.get('div#country').should('exist');
-      cy.get('textarea#description-field').should('exist');
-  
-      cy.get('button#update-provider').contains('Update Account');
-    });
-  
-    it('editing admin password form', () => {
-      cy.visit('/change');
-      cy.location().should((loc) => {
-        expect(loc.pathname).to.eq('/change');
-      });
-      cy.get('input#password-input-field').should('exist');
-      cy.get('input#confirm-password-input-field').should('exist');
-  
-      cy.get('button[type=submit]').contains('Set Password');
-    });
-  
-    it('visiting homepage', () => {
-      cy.visit('/');
-      cy.get('h2.work').contains('DataCite');
-      cy.get('li a.nav-link.active').contains('Settings');
-    });
-  
-    it('visiting info', () => {
-      cy.visit('/info');
-      cy.location().should((loc) => {
-        expect(loc.pathname).to.eq('/info');
-      });
-      cy.get('h2.work').contains('DataCite');
-      cy.get('li a.nav-link.active').contains('Info');
-    });
-  
-    it('visiting members', () => {
-      cy.visit('/providers');
-      cy.location().should((loc) => {
-        expect(loc.pathname).to.eq('/providers');
-      });
-      cy.get('h2.work').contains('DataCite');
-      cy.get('li a.nav-link.active').contains('Members');
-      cy.get('div#search').should('exist');
-      cy.get('div.panel.facets').should('exist');
-  
-      cy.get('a#add-provider').contains('Add Member');
-    });
-  
-    it('visiting repositories', () => {
-      cy.visit('/repositories');
-      cy.location().should((loc) => {
-        expect(loc.pathname).to.eq('/repositories');
-      });
-      cy.get('h2.work').contains('DataCite');
-      cy.get('li a.nav-link.active').contains('Repositories');
-      cy.get('div#search').should('exist');
-      cy.get('div.panel.facets').should('exist');
-  
-      // staff can't add repositories here (needs to go to provider first)
-      cy.get('a#add-repository').should('not.exist');
-    });
-  
-    it('visiting contacts', () => {
-      cy.visit('/contacts');
-      cy.location().should((loc) => {
-        expect(loc.pathname).to.eq('/contacts');
-      });
-      cy.get('h2.work').contains('DataCite');
-      cy.get('li a.nav-link.active').contains('Contacts');
-      cy.get('div#search').should('exist');
-      cy.get('div.panel.facets').should('exist');
-  
-      // staff can't add contacts here (needs to go to provider first)
-      cy.get('a#add-contact').should('not.exist');
-    });
-  
-    it('visiting prefixes', () => {
-      cy.visit('/prefixes');
-      cy.location().should((loc) => {
-        expect(loc.pathname).to.eq('/prefixes');
-      });
-      cy.get('h2.work').contains('DataCite');
-      cy.get('li a.nav-link.active').contains('Prefixes');
-      cy.get('div#search').should('exist');
-      cy.get('div.panel.facets').should('exist');
-  
-      cy.get('a#add-prefixes').contains('Add Prefixes');
-    });
-  
-    it('visiting prefix 10.80225', () => {
-      cy.visit('/prefixes/10.80225');
-      cy.location().should((loc) => {
-        expect(loc.pathname).to.eq('/prefixes/10.80225');
-      });
-      cy.get('div.alert-warning').contains('The page was not found.');
-    });
-  
-    it('visiting dois', () => {
-      cy.visit('/dois');
-      cy.location().should((loc) => {
-        expect(loc.pathname).to.eq('/dois');
-      });
-      cy.get('h2.work').contains('DataCite');
-      cy.get('li a.nav-link.active').contains('DOIs');
-      cy.get('div#search').should('exist');
-      cy.get('div.panel.facets').should('exist');
-  
-      // staff can't add doi here (needs to go to repository first)
-      cy.get('a#add-doi').should('not.exist');
-    });
-    */
+describe('Acceptance: consortium_admin, admin', () => {
+  const waitTimeBetIt = 1000;
+  const waitTime = 1000;
+  const waitTime1 = 1000;
+
+  beforeEach(() => {
+    cy.setCookie('_consent', 'true');
+    cy.setCookie('_fabrica', Cypress.env('consortium_admin_cookie'), { log: false });
+    cy.wait(waitTimeBetIt);
   });
-  
+
+  it('editing admin form', () => {
+    cy.visit('/providers/dc/edit');
+    cy.url().should('include', '/providers/dc/edit');
+    cy.wait(waitTime);
+
+    cy.get('input#member-id-field').should('exist');
+    cy.get('input#system-email-field').should('exist');
+    cy.get('input#website-field').should('exist');
+    cy.get('input#twitter-handle-field').should('exist');
+    cy.get('div#ror-id').should('exist');
+    cy.get('div#country').should('exist');
+    cy.get('textarea#description-field').should('exist');
+
+    cy.get('button#update-provider').contains('Update Member');
+  });
+
+  it.only('editing admin password form', () => {
+    cy.visit('/providers/dc/change');
+    cy.url().should('include', '/providers/dc/change');
+    cy.wait(waitTime);
+
+    cy.get('input#password-input-field').should('exist');
+    cy.get('input#confirm-password-input-field').should('exist');
+
+    cy.get('button[type=submit]').contains('Set Password');
+  });
+
+  it('is logged in', () => {
+    cy.visit('/');
+    cy.url().should('include', '/providers/dc');
+    cy.wait(waitTime);
+
+    cy.get('h2.work').contains('DataCite Consortium');
+    cy.get('ul.nav-tabs li.active a').should('contain', 'Settings');
+    cy.get('a#account_menu_link').should('contain', 'DC');
+  });
+
+  it('visiting homepage', () => {
+    cy.visit('/');
+    cy.url().should('include', '/providers/dc');
+    cy.wait(waitTime);
+
+    cy.get('h2.work').contains('DataCite Consortium');
+    cy.get('ul.nav-tabs li.active a').should('contain', 'Settings');
+    cy.get('a#account_menu_link').should('contain', 'DC');
+  });
+
+  it('visiting info', () => {
+    cy.visit('/providers/dc/info');
+    cy.url().should('include', '/providers/dc/info');
+    cy.wait(waitTime);
+
+    cy.get('h2.work').contains('DataCite Consortium');
+    cy.get('ul.nav-tabs li.active a').should('contain', 'Info');
+    cy.get('a#account_menu_link').should('contain', 'DC');
+  });
+
+  it('visiting settings', () => {
+    cy.visit('/');
+    cy.url().should('include', '/providers/dc');
+    cy.wait(waitTime);
+
+    cy.get('h2.work').contains('DataCite Consortium');
+    cy.get('ul.nav-tabs li.active a').should('contain', 'Settings');
+    cy.get('a#account_menu_link').should('contain', 'DC');
+  });
+
+  it('visiting consortium organizations', () => {
+    cy.visit('/providers/dc/organizations');
+    cy.url().should('include', '/providers/dc/organizations');
+    cy.wait(waitTime);
+
+    cy.get('h2.work').contains('DataCite Consortium');
+    cy.get('ul.nav-tabs li.active a').should('contain', 'Consortium Organizations');
+    cy.get('a#account_menu_link').should('contain', 'DC');
+
+    cy.get('div#search').should('exist');
+    cy.get('div.panel.facets').should('exist');
+  });
+
+  it('visiting contacts', () => {
+    cy.visit('/providers/dc/contacts');
+    cy.url().should('include', '/providers/dc/contacts');
+    cy.wait(waitTime);
+
+    cy.get('h2.work').contains('DataCite Consortium');
+    cy.get('ul.nav-tabs li.active a').should('contain', 'Contacts');
+    cy.get('a#account_menu_link').should('contain', 'DC');
+
+    cy.get('div#search').should('exist');
+    cy.get('div.panel.facets').should('exist');
+
+    cy.get('a#add-contact').should('exist');
+  });
+
+  it('visiting repositories', () => {
+    cy.visit('/providers/dc/repositories');
+    cy.url().should('include', '/providers/dc/repositories');
+    cy.wait(waitTime);
+
+    cy.get('h2.work').contains('DataCite Consortium');
+    cy.get('ul.nav-tabs li.active a').should('contain', 'Repositories');
+    cy.get('a#account_menu_link').should('contain', 'DC');
+
+    cy.get('div#search').should('exist');
+    cy.get('div.panel.facets').should('exist');
+
+    // staff can't add repositories here (needs to go to provider first)
+    cy.get('a#add-repository').should('not.exist');
+    cy.get('.alert').contains("New repositories can't be created from this page.");
+  });
+
+  it('visiting prefixes', () => {
+    cy.visit('/providers/dc/prefixes');
+    cy.url().should('include', '/providers/dc/prefixes');
+    cy.wait(waitTime);
+
+    cy.get('h2.work').contains('DataCite Consortium');
+    cy.get('ul.nav-tabs li.active a').should('contain', 'Prefixes');
+    cy.get('a#account_menu_link').should('contain', 'DC');
+ 
+    cy.get('div#search').should('exist');
+    cy.get('div.panel.facets').should('exist');
+
+    cy.get('a#add-prefixes').should('not.exist');
+    cy.get('.alert').contains("New prefixes can't be assigned from this page.");
+  });
+
+  it('visiting dois', () => {
+    cy.visit('/providers/dc/dois');
+    cy.url().should('include', '/providers/dc/dois');
+    cy.wait(waitTime);
+
+    cy.get('h2.work').contains('DataCite Consortium');
+    cy.get('ul.nav-tabs li.active a').should('contain', 'DOIs');
+    cy.get('a#account_menu_link').should('contain', 'DC');
+
+    cy.get('div#search').should('exist');
+    cy.get('div.panel.facets').should('exist');
+
+    // staff can't add doi here (needs to go to repository first)
+    cy.get('a#add-doi').should('not.exist');
+  });
+
+  it('visiting prefix 10.80225', () => {
+    cy.visit('/providers/dc/prefixes/10.80225');
+    cy.url().should('include', '/providers/dc/prefixes/10.80225');
+    cy.wait(waitTime);
+
+    cy.get('div.alert-warning').contains('The prefix was not found.');
+  });
+});
