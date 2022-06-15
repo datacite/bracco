bd6aafd5b5fa93e2a5d3889c58bd3e2f119abdc3
Suzanne Vogt <suzanne.vogt@datacite.org>
2022-06-15 17:34:15 -0400

    Prefix workflow. (Remove console.logs.)

--- app/services/prefixes.js
+++ app/services/prefixes.js
@@ -26,15 +26,7 @@ export default Service.extend({
               response.responseJSON = json;
               reject(response);
             } else {
-              // ENV.PREFIXES_AVAILABLE only for development/staging/testing.
-              let total = json.meta.total;
-              // let total = 0;
-              //if (ENV.PREFIXES_AVAILABLE !== null) {
-              //  total = ENV.PREFIXES_AVAILABLE;
-              //}
-              console.log("Prefixes available = " + total);
-              resolve(total);
-              // resolve(0);
+              resolve(json.meta.total);
             }
           } catch (SyntaxError) {
             response.responseText = text;
@@ -61,15 +53,7 @@ export default Service.extend({
               response.responseJSON = json;
               reject(response);
             } else {
-              // ENV.PREFIXES_AVAILABLE only for development/staging/testing.
-              let total = json.meta.total;
-              // let total = 0;
-              //if (ENV.PREFIXES_AVAILABLE !== null) {
-              //  total = ENV.PREFIXES_AVAILABLE;
-              //}
-              console.log("Provider prefixes available = " + total);
-              resolve(total);
-              // resolve(0);
+              resolve(json.meta.total);
             }
           } catch (SyntaxError) {
             response.responseText = text;
@@ -85,23 +69,15 @@ export default Service.extend({
   // Returns a promise.
   async available(provider_id = null) {
     let m = 0;
-
     if (provider_id) {
       m = await this.get_n_available_pp(provider_id);
-      console.log("PREFIXES.AVAILABLE() - AWAITING M!! " + m);
-      console.log("PREFIXES.AVAILABLE() - PROVIDER PREFIXES AVAILABLE FOR " + provider_id + ' = ' + m);
-      console.log("PREFIXES.AVAILABLE() M = " + m);
-    } else {
-      console.log("PREFIXES.AVAILABLE() - NO PROVIDER ID PROVIDED!!")
     }
 
     let n = await this.get_n_available();
-    console.log("N = " + n);
-    console.log("PREFIXES.AVAILABLE() - PROVIDER_ID - AVAILABLE: " + provider_id);
-    //return await this.get_n_available();
-    let o = m + n;
-    console.log('PREFIXES.AVAILABLE() - RETURNING M+N: ' + o);
+
     return m+n;
-    //return 0;
+
+    // FOR DEV/TESTING ONLY:
+    // return 0;
   }
 })
