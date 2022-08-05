a3dca9c2040a5b27035eeab4bdc6955981b135fe
Suzanne Vogt <suzanne.vogt@datacite.org>
2022-08-05 09:54:55 -0400

    Increase number of prefixes in assign prefix dropdown.

--- app/controllers/repositories/show/prefixes/new.js
+++ app/controllers/repositories/show/prefixes/new.js
@@ -17,7 +17,7 @@ export default Controller.extend({
     let self = this;
     let prefixes = [];
 
-    this.prefixes.get_prefixes(10, this.model.repository.get('provider.id'))
+    this.prefixes.get_prefixes(50, this.model.repository.get('provider.id'))
       .then((values) => {
         let provider = this.model.repository.provider;
 
