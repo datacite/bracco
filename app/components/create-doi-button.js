442519178c295c033e117bc11b99ef47df001d63
Suzanne Vogt <suzanne.vogt@datacite.org>
2022-08-09 17:15:47 -0400

    Fabrica Improvements - task 1 (create doi button).

---
+++ app/components/create-doi-button.js
@@ -0,0 +1,14 @@
+import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+
+export default Component.extend({
+  currentUser: service(),
+
+  classNames: [ 'panel', 'facets', 'add' ],
+
+  didReceiveAttrs() {
+    this._super(...arguments);
+
+    this.set('currentUser', this.currentUser);
+  },
+});
