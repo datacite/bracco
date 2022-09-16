14d87e8f061b9cd563869d8ac7529f5e2c97c58d
Mike Bennett <mbennett@datacite.org>
2022-09-16 21:02:33 +0100

    Add Classification Code to the subjects tests

--- tests/integration/components/doi-subjects-test.js
+++ tests/integration/components/doi-subjects-test.js
@@ -17,9 +17,18 @@ module('Integration | Component | doi subjects', function(hooks) {
     await click('#toggle-subjects');
     await click('#add-subject');
     await click('#add-subject');
-    let subjects = this.element.querySelectorAll('[data-test-subject-scheme]');
+    let subjectSchemes = this.element.querySelectorAll('[data-test-subject-scheme]');
 
-    await fillIn(subjects[0],'Fields of Science and Technology (FOS)');
-    assert.dom(subjects[0]).hasValue('Fields of Science and Technology (FOS)');
+    await fillIn(subjectSchemes[0],'Fields of Science and Technology (FOS)');
+    assert.dom(subjectSchemes[0]).hasValue('Fields of Science and Technology (FOS)');
+    
+    let subjectSchemeUris = this.element.querySelectorAll('[data-test-subject-uri]');
+    await fillIn(subjectSchemeUris[0],'https://example.com/fos');
+    assert.dom(subjectSchemeUris[0]).hasValue('https://example.com/fos');
+
+    let subjectSchemeCodes = this.element.querySelectorAll('[data-test-classification-code]');
+    await fillIn(subjectSchemeCodes[0],'ABC123');
+    assert.dom(subjectSchemeCodes[0]).hasValue('ABC123');
+
   });
 });
