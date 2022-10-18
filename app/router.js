5083aa300627b59509f52dcf1521e72be8679a12
Bryceson Laing <bryceson.laing@datacite.org>
2022-10-18 15:42:43 -0700

    Fabrica Improvements - task 2 (set info as index)

--- app/router.js
+++ app/router.js
@@ -13,7 +13,7 @@ Router.map(function () {
       this.route('edit');
       this.route('change');
       this.route('delete');
-      this.route('info');
+      this.route('settings');
       this.route('dois');
       this.route('prefixes', function () {
         this.route('show', { path: '/:prefix_id' }, function () {
@@ -38,7 +38,7 @@ Router.map(function () {
       this.route('edit');
       this.route('change');
       this.route('delete');
-      this.route('info');
+      this.route('settings');
       this.route('transfer-repository');
       this.route('dois', function () {
         this.route('new');
@@ -69,6 +69,7 @@ Router.map(function () {
   this.route('users', function () {
     this.route('show', { path: '/:user_id' }, function () {
       this.route('info');
+      this.route('settings');
       this.route('dois');
     });
   });
@@ -81,7 +82,7 @@ Router.map(function () {
   });
   this.route('edit');
   this.route('change');
-  this.route('info');
+  this.route('settings');
 
   this.route('about');
 
