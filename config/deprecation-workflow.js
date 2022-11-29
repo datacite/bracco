81d55eaa00980fad3e69ec35b4d2903905ce62b2
Suzanne Vogt <suzanne.vogt@datacite.org>
2022-11-29 00:22:54 -0500

    Fabrica-sw-update: silence a deprecation message (temporarily).

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -6,5 +6,6 @@ window.deprecationWorkflow.config = {
     { handler: 'silence', matchId: 'computed-property.volatile' },
     { handler: 'silence', matchId: 'computed-property.override' },
     { handler: 'silence', matchId: 'ember.globals-resolver' },
+    { handler: 'silence', matchId: 'implicit-modifier-manager-capabilities' },
   ],
 };
