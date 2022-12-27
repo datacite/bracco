3f44274e3fc0654bb3c6f72a22462a83aeead37c
Cody Ross <codycooperross@gmail.com>
2022-12-27 10:48:59 -0500

    Adds the ability to create a new Language in the Language property

--- app/components/doi-language.js
+++ app/components/doi-language.js
@@ -1,7 +1,7 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
 import ISO6391 from 'iso-639-1';
-
+import { isBlank } from '@ember/utils';
 
 const languageList = ISO6391.getAllNames();
 
@@ -12,8 +12,37 @@ export default Component.extend({
     return ISO6391.getName(this.get('model.language')) !== '' ? ISO6391.getName(this.get('model.language')) : this.get('model.language');
   }),
 
+  init(...args) {
+    this._super(...args);
+
+    this.selected = this.selected || [];
+  },
+
+  setLanguage(language) {
+    if (ISO6391.getCode(language)) {
+      this.model.set('language', ISO6391.getCode(language));
+    } else if (language) {
+      this.model.set('language', language);
+      this.get('model.language')
+    } else {
+      this.model.set('language', null);
+    }
+  },
 
   actions: {
+    createOnEnter(select, e) {
+      if (
+        e.keyCode === 13 &&
+        select.isOpen &&
+        !select.highlighted &&
+        !isBlank(select.searchText)
+      ) {
+        if (!this.selected.includes(select.searchText)) {
+          this.setLanguage(select.searchText)
+          select.actions.choose(select.searchText);
+        }
+      }
+    },
     searchLanguage(query) {
       let languages = languageList.filter(function(language) {
         return language.toLowerCase().startsWith(query.toLowerCase());
@@ -21,12 +50,8 @@ export default Component.extend({
       this.set('languages', languages);
     },
     selectLanguage(language) {
-      if (language) {
-        this.model.set('language', ISO6391.getCode(language));
-      } else {
-        this.model.set('language', null);
-      }
-      this.set('languages', languageList);
+      this.setLanguage(language);
+      this.set('languages', this.languageList);
     },
   },
 });
