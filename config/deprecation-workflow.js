ced63a3d17bbc71ed5d6bda21f886b82a4d32e9c
Suzanne Vogt <suzanne.vogt@datacite.org>
2023-01-31 16:44:30 -0500

    Fabrica-sw-update: silence compile loose mode errors, fix build loose mode error.

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -7,6 +7,8 @@ window.deprecationWorkflow.config = {
     { handler: 'silence', matchId: 'computed-property.override' },
     { handler: 'silence', matchId: 'ember.globals-resolver' },
     { handler: 'silence', matchId: 'implicit-modifier-manager-capabilities' },
-    { handler: 'silence', matchId: 'ember-cp-validations'}
+    { handler: 'silence', matchId: 'ember-cp-validations'},
+    { handler: 'silence', matchId: 'ember-cp-validations.inline-validator'},
+    { handler: 'silence', matchId: 'globals-resolver'}
   ],
 };
