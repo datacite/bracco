da466144a57378bdfe24073016b313b35ae1ed17
Suzanne Vogt <suzanne.vogt@datacite.org>
2023-06-11 19:18:16 -0400

    3.24

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -9,6 +9,13 @@ window.deprecationWorkflow.config = {
     { handler: 'silence', matchId: 'implicit-modifier-manager-capabilities' },
     { handler: 'silence', matchId: 'ember-cp-validations'},
     { handler: 'silence', matchId: 'ember-cp-validations.inline-validator'},
-    { handler: 'silence', matchId: 'globals-resolver'}
+    { handler: 'silence', matchId: 'globals-resolver'},
+    { handler: 'silence', matchId: 'ember-simple-auth.events.session-service' },
+    { handler: 'silence', matchId: 'ember-source.deprecation-without-for' },
+    { handler: 'silence', matchId: 'ember-source.deprecation-without-since' },
+    { handler: 'silence', matchId: 'ember.globals-resolver' },
+    { handler: 'silence', matchId: 'ember-simple-auth.mixins.unauthenticated-route-mixin' },
+    { handler: 'silence', matchId: 'ember-simple-auth.mixins.application-route-mixin' },
+    { handler: 'silence', matchId: 'ember-simple-auth.mixins.data-adapter-mixin' }
   ],
 };
