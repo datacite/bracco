0b23226e9660a821fc26d83fb20395c89b88ed44
Suzanne Vogt <suzanne.vogt@datacite.org>
2023-08-03 12:33:03 -0400

    Add uncaught error handler to allow us to run contacts tests.  Add in back in commented contact testing to pull_request.yml workflow.

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -18,3 +18,13 @@
 
 // Alternatively you can use CommonJS syntax:
 require('./commands')
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // we expect a 3rd party library error with the given message
+  // and don't want to fail the test so we return false
+  if (err.message.includes("Failed to update the 'id' for the RecordIdentifier")) {
+    return false
+  }
+  // we still want to ensure there are no other unexpected
+  // errors, so we let them fail the test
+})
