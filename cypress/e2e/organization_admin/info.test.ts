459b4b7da3b529027747bc422cbee1553d8be6b2
Suzanne Vogt <suzanne.vogt@datacite.org>
2023-11-01 18:25:34 -0400

    Fix failing test condition.

--- cypress/e2e/organization_admin/info.test.ts
+++ cypress/e2e/organization_admin/info.test.ts
@@ -182,9 +182,9 @@ describe('ACCEPTANCE: ORGANIZATION_ADMIN | INFO', () => {
     cy.visit('/providers/DATACITE');
     cy.url().should('include', '/providers/DATACITE').then(() => {
       
-      cy.wait(6000)
+      cy.wait(waitTime3)
       // Info page should be populated with non-zero graph data.
-      cy.get('.graphs > a').contains(/^0$/).should('not.exist')
+      cy.get('.graphs > a').contains(/^[0-9]+$/).should('exist')
     });
   });
 });
