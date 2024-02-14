b7e6c32eb851ad32444c5fbde63a69b92652790d
Suzanne Vogt <suzanne.vogt@datacite.org>
2024-02-14 10:40:56 -0500

    Deprecation: implicit-injections - flashMessages, store services/properties. Members tab.

--- app/controllers/providers/index.js
+++ app/controllers/providers/index.js
@@ -1,6 +1,9 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
+  flashMessages: service(),
+
   queryParams: [
     'query',
     'organization-id',
