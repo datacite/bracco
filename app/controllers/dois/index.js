fab6718dee9c45105117056a1524b564b7f17dcd
Suzanne Vogt <suzanne.vogt@datacite.org>
2024-02-14 12:53:29 -0500

    Deprecation: implicit-injections - flashMessages, store services/properties. Dois tab.

--- app/controllers/dois/index.js
+++ app/controllers/dois/index.js
@@ -1,6 +1,9 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
+  flashMessages: service(),
+
   queryParams: [ 'query', 'resource-type-id', 'provider-id', 'client-id', 'person-id', 'affiliation-id', 'prefix', 'year', 'created', 'registered', 'state', 'source', 'link-check-status', 'sort', 'schema-version', 'certificate', 'page', 'size', 'affiliation', 'publisher' ],
   query: null,
   'resource-type-id': null,
