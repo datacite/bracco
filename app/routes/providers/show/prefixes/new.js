04eafb227b82a3e3c48e085b34ebbf3fbc85f63b
Suzanne Vogt <suzanne.vogt@datacite.org>
2024-02-16 09:09:13 -0500

    Deprecations: provider prefixes tab. (implicit-injections, this-property-fallback)

--- app/routes/providers/show/prefixes/new.js
+++ app/routes/providers/show/prefixes/new.js
@@ -5,6 +5,7 @@ import { inject as service } from '@ember/service';
 export default Route.extend({
   can: service(),
   router: service(),
+  store: service(),
 
   model() {
     let provider = this.modelFor('providers/show');
