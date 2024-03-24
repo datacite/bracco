514ef67eff76348e0368705a9509550dcdc57542
Suzanne Vogt <suzanne.vogt@datacite.org>
2024-03-24 17:06:40 -0400

    Fixes for deprecations: this-property-fallback, implicit-injections.  CSS tweeks - various.

--- app/controllers/repositories/show.js
+++ app/controllers/repositories/show.js
@@ -1,6 +1,8 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
+  flashMessages: service(),
   // Enables showing newly assigned prefix on result page of repository creation.
   queryParams: ['assignedPrefix'],
   assignedPrefix: null
--- app/controllers/repositories/show/delete.js
+++ app/controllers/repositories/show/delete.js
@@ -4,6 +4,7 @@ import { inject as service } from '@ember/service';
 export default Controller.extend({
   store: service(),
   router: service(),
+  flashMessages: service(),
 
   actions: {
     submit() {
