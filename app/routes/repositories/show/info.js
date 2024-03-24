a6cad890dc49f9eeb96faaaf7168cdbb1de53651
Suzanne Vogt <suzanne.vogt@datacite.org>
2024-03-24 17:04:40 -0400

    Fixes deprecations: this-property-fallback, implicit-injections for repositories/show/settings.

--- app/routes/repositories/show/info.js
+++ app/routes/repositories/show/info.js
@@ -4,6 +4,7 @@ import { inject as service } from '@ember/service';
 export default Route.extend({
   can: service(),
   router: service(),
+  flashMessages: service(),
 
   model() {
     let self = this;
