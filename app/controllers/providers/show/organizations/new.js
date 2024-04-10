e4756ffba7aaf14a615d43477b0d8e994cf49a0a
Suzanne Vogt <suzanne.vogt@datacite.org>
2024-04-10 14:26:27 -0400

    Fix: fix provider logo upload by adding fileReader import where needed.

--- app/controllers/providers/show/organizations/new.js
+++ app/controllers/providers/show/organizations/new.js
@@ -7,6 +7,7 @@ import {
   focusAreaList,
   nonProfitStatusList
 } from 'bracco/models/provider';
+import FileReader from 'ember-file-upload/system/file-reader';
 
 export default Controller.extend({
   store: service(),
