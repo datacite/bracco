b5644e44b0371b6a3b08d8f776eb8f819060cc42
Suzanne Vogt <suzanne.vogt@datacite.org>
2024-07-08 13:37:12 -0400

    Ember v4.12: Temporarily manage the 'active' class on <BsNav> tabs (until we upgrade to bootstrap 5).

--- app/components/show-repository-tabs.js
+++ app/components/show-repository-tabs.js
@@ -6,6 +6,11 @@ export default Component.extend({
   didRender() {
     this._super(...arguments);
 
+    // Remove old active indicator
+    if (this.element.querySelector('li.nav-item.active')) {
+      this.element.querySelector('li.nav-item.active').classList.remove("active")
+    }
+
     if (this.element.querySelector('a.active.nav-link') == null) {
       this.element.querySelector('a.nav-link').classList.add("active")
     }
