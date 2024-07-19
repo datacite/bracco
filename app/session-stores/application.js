cb8640ec2b10a3d34d4e4230bb74e4f1c62fc5f0
Suzanne Vogt <suzanne.vogt@datacite.org>
2024-07-19 12:59:49 -0400

    Ember 4.12: add sameSite value to _fabrica cookie.

--- app/session-stores/application.js
+++ app/session-stores/application.js
@@ -1,5 +1,6 @@
 import CookieStore from 'ember-simple-auth/session-stores/cookie';
 
 export default CookieStore.extend({
-  cookieName: '_fabrica'
+  cookieName: '_fabrica',
+  sameSite: 'Lax'
 });
