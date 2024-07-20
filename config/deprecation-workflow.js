7cde088fbba181a7fc9787cef24674b85e272337
Suzanne Vogt <suzanne.vogt@datacite.org>
2024-07-19 21:22:01 -0400

    Ember 4.12: Clean up deprecation-workflow.js.

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -3,16 +3,12 @@
 window.deprecationWorkflow = window.deprecationWorkflow || {};
 window.deprecationWorkflow.config = {
   workflow: [
-    { handler: 'silence', matchId: 'deprecate-auto-location' },
     { handler: 'silence', matchId: 'ember-data:deprecate-early-static' },
     { handler: 'silence', matchId: 'ember-data:deprecate-model-reopenclass' },
     { handler: 'silence', matchId: 'ember-data:deprecate-normalize-modelname-helper' },
     { handler: 'silence', matchId: 'ember-data:deprecate-v1cache-store-apis' },
     { handler: 'silence', matchId: 'ember-data:rsvp-unresolved-async' },
     { handler: 'silence', matchId: 'ember-string.add-package' },
-    { handler: 'silence', matchId: 'remove-owner-inject' },
-    // deprecations that showed up in testing
-    { handler: 'silence', matchId: 'ember-polyfills.deprecate-assign' },
-    { handler: 'silence', matchId: 'deprecate-ember-error' }
+    { handler: 'silence', matchId: 'ember-polyfills.deprecate-assign' }
   ]
 };
