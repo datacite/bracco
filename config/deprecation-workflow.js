5cb752a678fcce066f754b14c520be21977fdb31
Suzanne Vogt <suzanne.vogt@datacite.org>
2024-07-31 13:29:27 -0400

    Deprecations: Allow reporting of ember-data:rsvp-unresolved-async.

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -7,7 +7,7 @@ window.deprecationWorkflow.config = {
     { handler: 'silence', matchId: 'ember-data:deprecate-model-reopenclass' },
     { handler: 'silence', matchId: 'ember-data:deprecate-normalize-modelname-helper' },
     { handler: 'silence', matchId: 'ember-data:deprecate-v1cache-store-apis' },
-    { handler: 'silence', matchId: 'ember-data:rsvp-unresolved-async' },
+    //{ handler: 'silence', matchId: 'ember-data:rsvp-unresolved-async' },
     // Keep this until ember 5.0.  This is an ember issue is corrected in ember 5.0.
     { handler: 'silence', matchId: 'ember-string.add-package'},
     // Keep this until ember-data 5.0. We are using ember-data 4.6.5. This deprecated code is in ember-data through 4.12.
