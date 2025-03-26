a1c6bf0bf00f5f7c2a55df5bd044be9dcc72a3b0
Suzanne Vogt <suzanne.vogt@datacite.org>
2025-03-26 16:46:01 -0400

    Sample conversion of classic component to glimmer component.

--- app/components/repository-sidebar.js
+++ app/components/repository-sidebar.js
@@ -1,17 +1,8 @@
-// Finish conversion of this component to a @glimmer component.
+import Component from '@glimmer/component';
 import { classNames } from '@ember-decorators/component';
 import { inject as service } from '@ember/service';
-import Component from '@ember/component';
 
 @classNames('panel', 'facets', 'add')
 export default class RepositorySidebar extends Component {
-  @service
-  currentUser;
-
-  didReceiveAttrs() {
-    super.didReceiveAttrs(...arguments);
-
-    this.currentUser = this.currentUser;
-
-  }
+  @service currentUser;
 }
